5a4a867aa65d30ac69322912c1aecd887e3ebd93
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2007-01-02 00:20:16 +0100

    add sort_headers to bookings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def sort_header(column, label = nil)
+    label ||= column.humanize
+    content_tag('th', label + ' ' + link_to(image_tag('up'), :order => column) + link_to(image_tag('down'), :order => column + ' DESC'))
+  end
 end
