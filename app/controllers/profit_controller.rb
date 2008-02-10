41e680432c3047f4b27eeff7ebf3fd5f91fc259e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2008-02-10 23:33:29 +0100

    implement period for profit

--- app/controllers/profit_controller.rb
+++ app/controllers/profit_controller.rb
@@ -5,6 +5,14 @@ class ProfitController < ApplicationController
   end
 
   def show
+    @start_date = params[:balance][:start_date] unless params[:balance].nil?
+    @end_date = params[:balance][:end_date] unless params[:balance].nil?
+    # use current date if not specified otherwise
+    @end_date ||= Date.today
+    @start_date ||= Date.today - 365
     @client = Client.find(:first)
+    Booking.with_scope(:find => {:conditions => ['value_date BETWEEN ? AND ?', @start_date, @end_date]}) do
+      render :action => :show
+    end
   end
 end
