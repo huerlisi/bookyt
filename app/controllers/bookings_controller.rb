001c966cba18dce07424a52a22c3db0d3fe70158
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2008-02-10 20:50:15 +0100

    use in place edit for value date in booking partial

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,8 @@
 class BookingsController < ApplicationController
   in_place_edit_for :booking, :title
   in_place_edit_for :booking, :comments
+  in_place_edit_for :booking, :in_place_amount
+  in_place_edit_for :booking, :in_place_value_date
 
   def index
     list
