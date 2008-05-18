1fcea19d22b1d6122718aaa3d3d3d77768a05e6c
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2008-05-18 20:57:20 +0200

    add after_update handler to day model adding bookings for cash and credit turnovers

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,2 +1,9 @@
 class Day < ActiveRecord::Base
+  def after_update
+    cash_booking = Booking.new(:title => 'Bareinnahmen', :debit_account_id => 21, :credit_account_id => 1, :amount => cash, :value_date => date)
+    cash_booking.save
+
+    credit_booking = Booking.new(:title => 'Kreditkarten Einnahmen', :debit_account_id => 21, :credit_account_id => 49, :amount => card_turnover, :value_date => date)
+    credit_booking.save
+  end
 end
