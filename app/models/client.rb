9e17c0c260ad53d431fd62b05fa7d7399ef9f742
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2009-04-25 19:29:32 +0200

    order client associations by number.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,12 +1,12 @@
 class Client < ActiveRecord::Base
   has_many :accounts
 
-  has_many :current_assets, :class_name => 'Account', :conditions => 'account_type_id = 1'
-  has_many :capital_assets, :class_name => 'Account', :conditions => 'account_type_id = 2'
-  has_many :outside_capital, :class_name => 'Account', :conditions => 'account_type_id = 3'
-  has_many :equity_capital, :class_name => 'Account', :conditions => 'account_type_id = 4'
-  has_many :expenses, :class_name => 'Account', :conditions => 'account_type_id = 5'
-  has_many :earnings, :class_name => 'Account', :conditions => 'account_type_id = 6'
+  has_many :current_assets, :class_name => 'Account', :conditions => 'account_type_id = 1', :order => 'number'
+  has_many :capital_assets, :class_name => 'Account', :conditions => 'account_type_id = 2', :order => 'number'
+  has_many :outside_capital, :class_name => 'Account', :conditions => 'account_type_id = 3', :order => 'number'
+  has_many :equity_capital, :class_name => 'Account', :conditions => 'account_type_id = 4', :order => 'number'
+  has_many :expenses, :class_name => 'Account', :conditions => 'account_type_id = 5', :order => 'number'
+  has_many :earnings, :class_name => 'Account', :conditions => 'account_type_id = 6', :order => 'number'
 
   def saldo(type)
     new_saldo = 0
