5de23218d9712e4df8c3b5620df442aef1350360
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-05-27 23:09:37 +0200

    [Rails 3] Add and update set of unproblematic files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '91d4505d90462738e1e527573148912e9f76eca5dad96739af1ea86cf407220a3a3e4b8d527fea0c70997e2f97f97e8eae7d32e077729a89c392c102e655653a'
