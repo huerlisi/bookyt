1f26e82e20ef1dd20f38998c3b3c241ff0259bbf
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-05-28 09:20:33 +0200

    [Rails 3] Mostly html_safe fixes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
   def sort_header(column, label = nil)
     label ||= column.humanize
-    content_tag('th', label + ' ' + link_to(image_tag('up.png', :class => 'edit_controls'), :order => column) + link_to(image_tag('down.png', :class => 'edit_controls'), :order => column + ' DESC'))
+    content_tag('th', label.html_safe + ' '.html_safe + link_to(image_tag('up.png', :class => 'edit_controls'), :order => column) + link_to(image_tag('down.png', :class => 'edit_controls'), :order => column + ' DESC'))
   end
 
   def currency_fmt(value)
@@ -10,6 +10,6 @@ module ApplicationHelper
   end
 
   def cu_to_s(value, unit = 'CHF')
-    "<span style=\"float: left\">#{unit}&nbsp;</span>" + number_to_currency(value, :unit => '')
+    "<span style=\"float: left\">#{unit}&nbsp;</span>".html_save + number_to_currency(value, :unit => '').html_save
   end
 end
