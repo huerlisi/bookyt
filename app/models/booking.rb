ba8385de72c66b50490784316a24cc2ad885f035
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-05-28 09:21:14 +0200

    Comment out unused file_column Booking.scan.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,7 @@ class Booking < ActiveRecord::Base
   belongs_to :debit_account, :foreign_key => 'debit_account_id', :class_name => "Account"
   belongs_to :credit_account, :foreign_key => 'credit_account_id', :class_name => "Account"
   
-  file_column :scan
+#  file_column :scan
 
   def rounded_amount
     if amount.nil?
