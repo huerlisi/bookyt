8fff5bd91e2b1e06f7ea6f789eb61037fa7b7f2e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-06-08 13:02:37 +0200

    Include ActionView::Helpers::NumberHelper in ApplicationHelper for number_with_precision.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  include ActionView::Helpers::NumberHelper
+  
   def sort_header(column, label = nil)
     label ||= column.humanize
     content_tag('th', label.html_safe + ' '.html_safe + link_to(image_tag('up.png', :class => 'edit_controls'), :order => column) + link_to(image_tag('down.png', :class => 'edit_controls'), :order => column + ' DESC'))
