60994b63b897b7a516726106d4011f94c9cc5ba1
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-06-13 13:40:49 +0200

    Bring Account and Booking model closer to those of has_accounts plugin.

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,30 +5,53 @@ class Account < ActiveRecord::Base
   has_many :credit_bookings, :class_name => "Booking", :foreign_key => "credit_account_id"
   has_many :debit_bookings, :class_name => "Booking", :foreign_key => "debit_account_id"
 
-  has_many :bookings, :class_name => "Booking", :finder_sql => 'SELECT * FROM bookings WHERE credit_account_id = #{id} OR debit_account_id = #{id} ORDER BY value_date'
+  def bookings
+    Booking.by_account(id)
+  end
   
-  # Scope
-  default_scope :order => 'code'
+  def turnover(selector = Date.today, inclusive = true)
+    if selector.is_a? Range or selector.is_a? Array
+      if selector.first.is_a? Booking
+        equality = "=" if inclusive
+        if selector.first.value_date == selector.last.value_date
+          condition = ["value_date = :value_date AND id >#{equality} :first_id AND id <#{equality} :last_id", {
+            :value_date => selector.first.value_date,
+            :first_id => selector.first.id,
+            :last_id => selector.last.id
+          }]
+        else
+          condition = ["(value_date > :first_value_date AND value_date < :latest_value_date) OR (value_date = :first_value_date AND id >#{equality} :first_id) OR (value_date = :latest_value_date AND id <#{equality} :last_id)", {
+            :first_value_date => selector.first.value_date,
+            :latest_value_date => selector.last.value_date,
+            :first_id => selector.first.id,
+            :last_id => selector.last.id
+          }]
+        end
+      elsif
+        # TODO support inclusive param
+        condition = {:value_date => selector}
+      end
+    else
+      if selector.is_a? Booking
+        equality = "=" if inclusive
+        condition = ["(value_date < :value_date) OR (value_date = :value_date AND id <#{equality} :id)", {:value_date => selector.value_date, :id => selector.id}]
+      else
+        equality = "=" if inclusive
+        condition = ["value_date <#{equality} ?", selector]
+      end
+    end
+
+    credit_amount = credit_bookings.sum(:amount, :conditions => condition)
+    debit_amount = debit_bookings.sum(:amount, :conditions => condition)
+    
+    [credit_amount || 0.0, debit_amount || 0.0]
+  end
   
-  def saldo
-    credit_amount = credit_bookings.sum(:amount)
-    debit_amount = debit_bookings.sum(:amount)
-    
-    credit_amount ||= 0
-    debit_amount ||= 0
-    
+  def saldo(selector = Date.today, inclusive = true)
+    credit_amount, debit_amount = turnover(selector, inclusive)
+
     return credit_amount - debit_amount
   end
-
-  def saldo=(value)
-    booking = Booking.new
-    amount = value.to_f
-    if amount > 0
-      booking.amount = amount
-      booking.credit_account_id = self.id
-    elsif amount < 0
-      booking.amount = amount.abs
-      booking.debit_account_id = self.id
-    end
-  end
+  # Scope
+  default_scope :order => 'code'
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,61 @@
 class Booking < ActiveRecord::Base
   include ApplicationHelper
   
+  # Associations
   belongs_to :debit_account, :foreign_key => 'debit_account_id', :class_name => "Account"
   belongs_to :credit_account, :foreign_key => 'credit_account_id', :class_name => "Account"
   
+  # Scoping
+  named_scope :by_value_date, lambda {|value_date| { :conditions => { :value_date => value_date } } }
+  named_scope :by_value_period, lambda {|from, to| { :conditions => { :value_date => from..to } } }
+  
+  named_scope :by_account, lambda {|account_id|
+    { :conditions => ["debit_account_id = :account_id OR credit_account_id = :account_id", {:account_id => account_id}] }
+  } do
+    # Returns array of all booking titles.
+    def titles
+      find(:all, :group => :title).map{|booking| booking.title}
+    end
+    
+    # Statistics per booking title.
+    #
+    # The statistics are an array of hashes with keys title, count, sum, average.
+    def statistics
+      find(:all, :select => "title, count(*) AS count, sum(amount) AS sum, avg(amount) AS avg", :group => :title).map{|stat| stat.attributes}
+    end
+  end
+
 #  file_column :scan
+
+  # Standard methods
+  def to_s(format = :default)
+    case format
+    when :short
+      "#{value_date.strftime('%d.%m.%Y')}: #{credit_account.code} / #{debit_account.code} CHF #{amount_as_string} "
+    else
+      "#{value_date.strftime('%d.%m.%Y')}: #{credit_account.title} (#{credit_account.code}) an #{debit_account.title} (#{debit_account.code}) CHF #{amount_as_string}, #{title} " +
+        (comments.blank? ? "" :"(#{comments})")
+    end
+  end
+
+  # Helpers
+  def accounted_amount(account)
+    if credit_account == account
+      return amount
+    elsif debit_account == account
+      return -(amount)
+    else
+      return 0.0
+    end
+  end
+
+  def amount_as_string
+    '%0.2f' % amount
+  end
+  
+  def amount_as_string=(value)
+    self.amount = value
+  end
 
   def rounded_amount
     if amount.nil?
