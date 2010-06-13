5f6786f00e5f0f5edd1cde823e1c839228b459c6
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-06-14 00:00:53 +0200

    Use inherited resources for bookings controller.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,69 +1,18 @@
-class BookingsController < ApplicationController
+class BookingsController < InheritedResources::Base
   in_place_edit_for :booking, :title
   in_place_edit_for :booking, :comments
   in_place_edit_for :booking, :in_place_amount
   in_place_edit_for :booking, :in_place_value_date
 
   def index
-    list
-    render :action => 'list'
-  end
-
-  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-  verify :method => :post, :only => [ :destroy, :create, :update ],
-         :redirect_to => { :action => :list }
-
-  def list
     order_by = params[:order] || 'value_date'
     @bookings = Booking.paginate :page => params[:page], :per_page => 100, :order => order_by
-  end
-
-  def show
-    @booking = Booking.find(params[:id])
-  end
-
-  def new
-    @booking = Booking.new
-  end
-
-  def create
-    @booking = Booking.new(params[:booking])
-    if @booking.save
-      flash[:notice] = 'Booking was successfully created.'
-      redirect_to :action => 'new'
-    else
-      render :action => 'new'
-    end
-  end
-
-  def edit
-    @booking = Booking.find(params[:id])
-  end
+    index!
 
   def copy
     @booking = Booking.find(params[:id])
     new_booking = @booking.clone
     new_booking.save
     render :partial => 'booking', :object => new_booking
-  end
-  
-  def update
-    @booking = Booking.find(params[:id])
-    if @booking.update_attributes(params[:booking])
-      flash[:notice] = 'Booking was successfully updated.'
-      redirect_to :action => 'list'
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    Booking.find(params[:id]).destroy
-    redirect_to :action => 'list'
-  end
-
-  def destroy_inline
-    Booking.find(params[:id]).destroy
-    render :text => ""
   end
 end
