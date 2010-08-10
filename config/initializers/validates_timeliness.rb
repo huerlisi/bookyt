4aeb837826f4f8ddf7f65c109f1c0a8dbd05217d
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-10 11:10:59 +0200

    Add validates_timeliness plugin.

---
+++ config/initializers/validates_timeliness.rb
@@ -0,0 +1,19 @@
+ValidatesTimeliness.setup do |config|
+  # Add validation helpers to these classes
+  config.extend_classes = [ ActiveRecord::Base ]
+  #
+  # Set the dummy date part for a time type values.
+  # config.dummy_date_for_time_type = [ 2000, 1, 1 ]
+  #
+  # Ignore errors when restriction options are evaluated
+  # config.ignore_restriction_errors = false
+  #
+  # Re-display invalid values in date/time selects
+  # config.enable_date_time_select_extension!
+  #
+  # Handle multiparameter date/time values strictly
+  # config.enable_multiparameter_extension!
+  #
+  # Shorthand date and time symbols for restrictions
+  # config.restriction_shorthand_symbols.update()
+end
