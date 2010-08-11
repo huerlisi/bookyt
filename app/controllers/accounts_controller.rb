d63a3c293184f03528ffeb0049f61cd3a7f85941
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-11 12:59:54 +0200

    Implement simple debit/credit booking filter for account view.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -13,7 +13,14 @@ class AccountsController < InheritedResources::Base
   
   def show
     @account = Account.find(params[:id])
-    @bookings = Booking.by_account(@account).paginate(:page => params['page'], :per_page => 20, :order => 'value_date, id')
+    @bookings = Booking.by_account(@account)
+    if params[:only_credit_bookings]
+      @bookings = @bookings.where(:credit_account_id => @account.id)
+    end
+    if params[:only_debit_bookings]
+      @bookings = @bookings.where(:debit_account_id => @account.id)
+    end
+    @bookings = @bookings.paginate(:page => params['page'], :per_page => 20, :order => 'value_date, id')
         
     show!
   end
