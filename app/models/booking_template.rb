58b4fc3e6cd36ee2517d53f6442635099c0e42ca
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-12 09:23:09 +0200

    BookingTemplate.to_s now handles partial information.

--- app/models/booking_template.rb
+++ app/models/booking_template.rb
@@ -2,4 +2,21 @@ class BookingTemplate < ActiveRecord::Base
   # Associations
   belongs_to :debit_account, :foreign_key => 'debit_account_id', :class_name => "Account"
   belongs_to :credit_account, :foreign_key => 'credit_account_id', :class_name => "Account"
+
+  # Standard methods
+  def to_s(format = :default)
+    case format
+    when :short
+      "#{value_date}: #{credit_account.code} / #{debit_account.code} CHF #{amount_as_string} "
+    else
+      "%s: %s an %s CHF %s, %s (%s)" % [
+        value_date ? value_date : '?',
+        credit_account ? "#{credit_account.title} (#{credit_account.code})" : '?',
+        debit_account ? "#{debit_account.title} (#{debit_account.code})" : '?',
+        amount ? "%0.2f" % amount : '?',
+        title ? title : '?',
+        comments ? comments : '?'
+      ]
+    end
+  end
 end
--- test/unit/booking_template_test.rb
+++ test/unit/booking_template_test.rb
@@ -1,8 +1,26 @@
 require 'test_helper'
 
 class BookingTemplateTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "to_s handles templates with few information" do
+    template = BookingTemplate.new
+    assert_equal "?: ? an ? CHF ?, ? (?)", template.to_s
+    
+    template.title = "Test"
+    assert_equal "?: ? an ? CHF ?, Test (?)", template.to_s
+
+    template.value_date = '2010-03-02'
+    assert_equal "02.03.2010: ? an ? CHF ?, Test (?)", template.to_s
+
+    template.credit_account = accounts(:ubs)
+    assert_equal "02.03.2010: UBS (1002) an ? CHF ?, Test (?)", template.to_s
+
+    template.debit_account = accounts(:postfinance)
+    assert_equal "02.03.2010: UBS (1002) an Postfinance (1001) CHF ?, Test (?)", template.to_s
+
+    template.comments = "Comment"
+    assert_equal "02.03.2010: UBS (1002) an Postfinance (1001) CHF ?, Test (Comment)", template.to_s
+
+    template.amount = "77.3"
+    assert_equal "02.03.2010: UBS (1002) an Postfinance (1001) CHF 77.30, Test (Comment)", template.to_s
   end
 end
