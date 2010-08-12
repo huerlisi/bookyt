c24852d659139cec350b04ac707d61e869f9c845
Roman <roman.simecek@screenconcept.ch>
2010-08-12 11:58:44 +0200

    Seeds with Accounts extended.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+Account.create!([
+  {:code => "1000", :title => "Kasse"},
+  {:code => "1100", :title => "Debitoren"},
+  {:code => "1210", :title => "Lager Medikamente"},
+  {:code => "3200", :title => "Dienstleistungsertrag"},
+  {:code => "3900", :title => "Debitorenverlust"},
+  {:code => "4000", :title => "Aufwand Medikamente"},
+])
