f147343f10f05cb8cf04d29024eda52941c93391
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-12 08:44:38 +0200

    Use haml instead of erb for booking_templates.

--- app/controllers/booking_templates_controller.rb
+++ app/controllers/booking_templates_controller.rb
@@ -1,2 +1,7 @@
 class BookingTemplatesController < InheritedResources::Base
+  def index
+    order_by = params[:order] || 'value_date'
+    @booking_templates = BookingTemplate.paginate :page => params[:page], :per_page => 100, :order => order_by
+    index!
+  end
 end
--- app/models/booking_template.rb
+++ app/models/booking_template.rb
@@ -1,2 +1,5 @@
 class BookingTemplate < ActiveRecord::Base
+  # Associations
+  belongs_to :debit_account, :foreign_key => 'debit_account_id', :class_name => "Account"
+  belongs_to :credit_account, :foreign_key => 'credit_account_id', :class_name => "Account"
 end
