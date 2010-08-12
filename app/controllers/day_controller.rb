01585c45b35c6205c14e450bb55c886793a642bd
Roman <roman.simecek@screenconcept.ch>
2010-08-12 11:12:34 +0200

    Day cash up form bugfixed.

--- app/controllers/day_controller.rb
+++ app/controllers/day_controller.rb
@@ -44,9 +44,7 @@ class DayController < ApplicationController
     bills = params[:cash_register].select { |a| a[0].to_f > 5 }
     mint = params[:cash_register].select { |a| a[0].to_f <= 5 }
     
-    cash = bills.map { |a| a[0].to_f * a[1].to_f }.sum
-    cash += mint.map { |a| a[1].to_f }.sum
-    
-    render :inline => "<td style='font-weight: bolder'>#{cash}</td>"
+    @cash = bills.map { |a| a[0].to_f * a[1].to_f }.sum
+    @cash += mint.map { |a| a[1].to_f }.sum
   end
 end
