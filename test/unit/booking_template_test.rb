26f22a51166678de8efc27aca499621afbe27042
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-12 09:27:22 +0200

    Use seperate tests for BookingTemplate.to_s

--- test/unit/booking_template_test.rb
+++ test/unit/booking_template_test.rb
@@ -1,26 +1,41 @@
 require 'test_helper'
 
 class BookingTemplateTest < ActiveSupport::TestCase
-  test "to_s handles templates with few information" do
-    template = BookingTemplate.new
-    assert_equal "?: ? an ? CHF ?, ? (?)", template.to_s
+  setup do
+    @template = BookingTemplate.new
+  end
+  
+  test "to_s handles all empty template" do
+    assert_equal "?: ? an ? CHF ?, ? (?)", @template.to_s
+  end
     
-    template.title = "Test"
-    assert_equal "?: ? an ? CHF ?, Test (?)", template.to_s
-
-    template.value_date = '2010-03-02'
-    assert_equal "02.03.2010: ? an ? CHF ?, Test (?)", template.to_s
-
-    template.credit_account = accounts(:ubs)
-    assert_equal "02.03.2010: UBS (1002) an ? CHF ?, Test (?)", template.to_s
-
-    template.debit_account = accounts(:postfinance)
-    assert_equal "02.03.2010: UBS (1002) an Postfinance (1001) CHF ?, Test (?)", template.to_s
-
-    template.comments = "Comment"
-    assert_equal "02.03.2010: UBS (1002) an Postfinance (1001) CHF ?, Test (Comment)", template.to_s
-
-    template.amount = "77.3"
-    assert_equal "02.03.2010: UBS (1002) an Postfinance (1001) CHF 77.30, Test (Comment)", template.to_s
+  test "to_s handles template with title" do
+    @template.title = "Test"
+    assert_equal "?: ? an ? CHF ?, Test (?)", @template.to_s
+  end
+    
+  test "to_s handles template with value date" do
+    @template.value_date = '2010-03-02'
+    assert_equal "02.03.2010: ? an ? CHF ?, ? (?)", @template.to_s
+  end
+    
+  test "to_s handles template with credit_account" do
+    @template.credit_account = accounts(:ubs)
+    assert_equal "?: UBS (1002) an ? CHF ?, ? (?)", @template.to_s
+  end
+    
+  test "to_s handles template with debit_account" do
+    @template.debit_account = accounts(:postfinance)
+    assert_equal "?: ? an Postfinance (1001) CHF ?, ? (?)", @template.to_s
+  end
+    
+  test "to_s handles template with comments" do
+    @template.comments = "Comment"
+    assert_equal "?: ? an ? CHF ?, ? (Comment)", @template.to_s
+  end
+    
+  test "to_s handles template with amount" do
+    @template.amount = "77.3"
+    assert_equal "?: ? an ? CHF 77.30, ? (?)", @template.to_s
   end
 end
