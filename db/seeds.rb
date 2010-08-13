7a8ab072d5e470ae47f691acfc5be4cd8e73c6a4
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-13 12:11:41 +0200

    Add booking templates for daily cash up bookings.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,15 @@
 #
 Account.create!([
   {:code => "1000", :title => "Kasse"},
+  {:code => "1010", :title => "Kasse Laden"},
   {:code => "1100", :title => "Debitoren"},
-  {:code => "1210", :title => "Lager Medikamente"},
+  {:code => "1021", :title => "Lager Medikamente"},
   {:code => "3200", :title => "Dienstleistungsertrag"},
   {:code => "3900", :title => "Debitorenverlust"},
-  {:code => "4000", :title => "Aufwand Medikamente"},
+])
+
+BookingTemplate.create!([
+  {:title => "Bareinnahmen", :credit_account => Account.where(:code => "3200").first, :debit_account => Account.where(:code => "1010").first},
+  {:title => "Kreditkarten Einnahmen", :credit_account => Account.where(:code => "3200").first, :debit_account => Account.where(:code => "1021").first},
+  {:title => "Barausgabe", :credit_account => Account.where(:code => "1000").first, :debit_account => Account.where(:code => "1010").first},
 ])
