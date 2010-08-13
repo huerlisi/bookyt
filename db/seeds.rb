072071dfe1724360ce3521893c87edfc2c30e734
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-13 14:59:27 +0200

    Add accounts and booking templates for cash up bookings.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,9 @@
 Account.create!([
   {:code => "1000", :title => "Kasse"},
   {:code => "1010", :title => "Kasse Laden"},
+  {:code => "1021", :title => "EFT Kontokorrent"},
   {:code => "1100", :title => "Debitoren"},
-  {:code => "1021", :title => "Lager Medikamente"},
+  {:code => "2001", :title => "Ausstehende Gutscheine"},
   {:code => "3200", :title => "Dienstleistungsertrag"},
   {:code => "3900", :title => "Debitorenverlust"},
 ])
@@ -13,5 +14,8 @@ Account.create!([
 BookingTemplate.create!([
   {:title => "Bareinnahmen", :credit_account => Account.where(:code => "3200").first, :debit_account => Account.where(:code => "1010").first},
   {:title => "Kreditkarten Einnahmen", :credit_account => Account.where(:code => "3200").first, :debit_account => Account.where(:code => "1021").first},
-  {:title => "Barausgabe", :credit_account => Account.where(:code => "1000").first, :debit_account => Account.where(:code => "1010").first},
+  {:title => "Barausgabe", :credit_account => Account.where(:code => "1010").first, :debit_account => Account.where(:code => "1000").first},
+  {:title => "Kredit", :credit_account => Account.where(:code => "3200").first, :debit_account => Account.where(:code => "1100").first},
+  {:title => "Gutschein ausgestellt", :credit_account => Account.where(:code => "2001").first, :debit_account => Account.where(:code => "1010").first},
+  {:title => "Gutschein eingelöst", :credit_account => Account.where(:code => "3200").first, :debit_account => Account.where(:code => "2001").first},
 ])
