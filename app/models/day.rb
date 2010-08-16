15a8fd21433b2f3d7efc46374ec92f77cdb508d8
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-16 14:36:56 +0200

    Create bookings for credit turnover and expenses in cash up.

--- app/models/day.rb
+++ app/models/day.rb
@@ -5,5 +5,7 @@ class Day < ActiveRecord::Base
   def create_bookings
     BookingTemplate.create_booking('day:cash', :amount => cash, :value_date => date)
     BookingTemplate.create_booking('day:card turnover', :amount => card_turnover, :value_date => date)
+    BookingTemplate.create_booking('day:expenses', :amount => expenses, :value_date => date)
+    BookingTemplate.create_booking('day:credit_turnover', :amount => credit_turnover, :value_date => date)
   end
 end
