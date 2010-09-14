49b88e952853efebde3634344714e30c6f89dadc
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-09-14 23:34:29 +0200

    New db schema after has_vcard migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100820063621) do
+ActiveRecord::Schema.define(:version => 20100914211641) do
 
   create_table "account_types", :force => true do |t|
     t.string "name",  :limit => 100
@@ -37,9 +37,23 @@ ActiveRecord::Schema.define(:version => 20100820063621) do
   add_index "accounts", ["holder_id", "holder_type"], :name => "index_accounts_on_holder_id_and_holder_type"
   add_index "accounts", ["type"], :name => "index_accounts_on_type"
 
+  create_table "addresses", :force => true do |t|
+    t.string  "post_office_box",  :limit => 50
+    t.string  "extended_address", :limit => 50
+    t.string  "street_address",   :limit => 50
+    t.string  "locality",         :limit => 50
+    t.string  "region",           :limit => 50
+    t.string  "postal_code",      :limit => 50
+    t.string  "country_name",     :limit => 50
+    t.integer "vcard_id"
+    t.string  "address_type"
+  end
+
+  add_index "addresses", ["vcard_id"], :name => "addresses_vcard_id_index"
+
   create_table "booking_templates", :force => true do |t|
     t.string   "title"
-    t.decimal  "amount",            :precision => 10, :scale => 0
+    t.decimal  "amount"
     t.integer  "credit_account_id"
     t.integer  "debit_account_id"
     t.date     "value_date"
@@ -51,15 +65,15 @@ ActiveRecord::Schema.define(:version => 20100820063621) do
 
   create_table "bookings", :force => true do |t|
     t.string  "title",             :limit => 100
-    t.decimal "amount",                           :precision => 10, :scale => 0
+    t.decimal "amount"
     t.integer "credit_account_id"
     t.integer "debit_account_id"
     t.date    "value_date"
-    t.text    "comments"
+    t.text    "comments",          :limit => 1000, :default => ""
     t.string  "scan"
-    t.string  "debit_currency",                                                  :default => "CHF"
-    t.string  "credit_currency",                                                 :default => "CHF"
-    t.float   "exchange_rate",                                                   :default => 1.0
+    t.string  "debit_currency",                    :default => "CHF"
+    t.string  "credit_currency",                   :default => "CHF"
+    t.float   "exchange_rate",                     :default => 1.0
   end
 
   create_table "clients", :force => true do |t|
@@ -81,20 +95,39 @@ ActiveRecord::Schema.define(:version => 20100820063621) do
     t.decimal  "discount",        :default => 0.0
   end
 
+  create_table "people", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "phone_numbers", :force => true do |t|
+    t.string  "number",            :limit => 50
+    t.string  "phone_number_type", :limit => 50
+    t.integer "vcard_id"
+    t.integer "object_id"
+    t.string  "object_type"
+  end
+
+  add_index "phone_numbers", ["object_id", "object_type"], :name => "index_phone_numbers_on_object_id_and_object_type"
+  add_index "phone_numbers", ["phone_number_type"], :name => "index_phone_numbers_on_phone_number_type"
+  add_index "phone_numbers", ["vcard_id"], :name => "phone_numbers_vcard_id_index"
+
   create_table "products", :force => true do |t|
   end
 
   create_table "vcards", :force => true do |t|
-    t.string  "full_name",          :limit => 50
-    t.string  "nickname",           :limit => 50
-    t.integer "address"
-    t.integer "billing_address_id"
-    t.integer "address_id"
-    t.string  "family_name",        :limit => 50
-    t.string  "given_name",         :limit => 50
-    t.string  "additional_name",    :limit => 50
-    t.string  "honorific_prefix",   :limit => 50
-    t.string  "honorific_suffix",   :limit => 50
+    t.string  "full_name",        :limit => 50
+    t.string  "nickname",         :limit => 50
+    t.string  "family_name",      :limit => 50
+    t.string  "given_name",       :limit => 50
+    t.string  "additional_name",  :limit => 50
+    t.string  "honorific_prefix", :limit => 50
+    t.string  "honorific_suffix", :limit => 50
+    t.boolean "active",                         :default => true
+    t.integer "object_id"
+    t.string  "object_type"
   end
+
+  add_index "vcards", ["object_id", "object_type"], :name => "index_vcards_on_object_id_and_object_type"
 
 end
