4b6530e0bbc51e954e80cbad226dca4052fd8520
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-09-15 01:40:46 +0200

    Move vcard initializer from employee to person.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,14 +3,6 @@ class Employee < Person
   has_many :employments
   has_many :employers, :through => :employments
 
-  # Constructor
-  def initialize(attributes = nil)
-    super
-
-    build_vcard unless vcard
-    vcard.build_address unless vcard.address
-  end
-
   # Helpers
   def to_s
     "%s (%s)" % [vcard.try(:full_name), date_of_birth]
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,15 @@
 class Person < ActiveRecord::Base
+  # Associations
   has_many :vcards, :as => :object
   has_one :vcard, :as => :object
 
   accepts_nested_attributes_for :vcard
+
+  # Constructor
+  def initialize(attributes = nil)
+    super
+
+    build_vcard unless vcard
+    vcard.build_address unless vcard.address
+  end
 end
