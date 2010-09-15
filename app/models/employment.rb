8c094e1c83992dc27049068112e0cdbc504b75e1
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-09-15 22:16:50 +0200

    Implement Employment.to_s.

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -6,4 +6,9 @@ class Employment < ActiveRecord::Base
   # Validations
   validates_presence_of :employee, :employer
   validates_date :duration_from, :duration_to, :allow_nil => true, :allow_blank => true
+
+  # Helpers
+  def to_s
+    "%s bei %s von %s - %s" % [employee, employer, duration_from, duration_to]
+  end
 end
