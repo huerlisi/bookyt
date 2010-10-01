2903b55dc3c47eb908ba329f66a9c95a34f449f8
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-10-02 00:42:24 +0200

    Add employee selection to user form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :person_id
 
   # Associations
   belongs_to :person
