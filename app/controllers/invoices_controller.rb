e1b0e51043d548989af934853a1bc067bbdca9c7
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-10-11 13:17:33 +0200

    Use "current" organization as default biller for invoices.

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -6,4 +6,13 @@ class InvoicesController < InheritedResources::Base
     def collection
       @invoices ||= end_of_association_chain.paginate(:page => params[:page])
     end
+
+  public
+  def new
+    invoice_params = params[:invoice] || {}
+    invoice_params.merge!(:company_id => current_user.person.employers.first.id)
+    @invoice = Invoice.new(invoice_params)
+    
+    new!
+  end
 end
