229dcaacdef30fe8c466725cfa3faaea38e3da6b
Roman <roman.simecek@screenconcept.ch>
2010-10-12 11:31:38 +0200

    Replaced prototype with jQuery. Some JS function must be still refactored.

--- public/javascripts/cyt.js
+++ public/javascripts/cyt.js
@@ -1,12 +1,17 @@
 // Autofocus element having attribute data-autofocus
-document.observe("dom:loaded", function() {
-  elements = $$("*[data-autofocus]");
-  elements.first().activate();
-});
+function addAutofocusBehaviour() {
+  $('*[data-autofocus=true]').first().focus();
+};
+
+function addDatePickerBehaviour() {
+  $('*[date-picker=true]').each(function(){
+    $(this).datepicker();
+  });
+}
 
 // Linkify containers having attribute data-href-container
-document.observe("dom:loaded", function() {
-  var elements = $$("[data-href-container]");
+function addLinkifyContainersBehaviour() {
+  var elements = $('*[data-href-container]');
   elements.each(function(element) {
     var container = element.up(element.readAttribute('data-href-container'));
     container.style.cursor = "pointer";
@@ -29,46 +34,54 @@ document.observe("dom:loaded", function() {
       return false;
     }
   });
-});
+};
 
 // Javascript Highlighter
 // Fixed version of comment in
 // http://stackoverflow.com/questions/1650389/prototype-js-highlight-words-dom-traversing-correctly-and-efficiently
-Element.addMethods({
-  highlight: function(element, term, className) {   
-    function innerHighlight(element, term, className) {
-      className = className || 'highlight';
-      term = (term || '').toUpperCase();
+// TODO: Check with Simon Hürlimann if this function is still in use and rewrite it for jQuery.
+//Element.addMethods({
+//  highlight: function(element, term, className) {
+//    function innerHighlight(element, term, className) {
+//      className = className || 'highlight';
+//      term = (term || '').toUpperCase();
+//
+//      var skip = 0;
+//      if ($(element).nodeType == 3) {
+//        var pos = element.data.toUpperCase().indexOf(term);
+//        if (pos >= 0) {
+//          var middlebit = element.splitText(pos),
+//              endbit = middlebit.splitText(term.length),
+//              middleclone = middlebit.cloneNode(true),
+//              spannode = document.createElement('span');
+//
+//          spannode.className = className;
+//          spannode.appendChild(middleclone);
+//          middlebit.parentNode.replaceChild(spannode, middlebit);
+//          skip = 1;
+//        }
+//      }
+//      else if (element.nodeType == 1 && element.childNodes && !/(script|style)/i.test(element.tagName)) {
+//        for (var i = 0; i < element.childNodes.length; ++i)
+//          i += innerHighlight(element.childNodes[i], term, className);
+//      }
+//      return skip;
+//    }
+//    innerHighlight(element, term, className);
+//    return element;
+//  },
+//  removeHighlight: function(element, term, className) {
+//    className = className || 'highlight';
+//    $(element).select("span."+className).each(function(e) {
+//      e.parentNode.replaceChild(e.firstChild, e);
+//    });
+//    return element;
+//  }
+//});
 
-      var skip = 0;
-      if ($(element).nodeType == 3) {
-        var pos = element.data.toUpperCase().indexOf(term);
-        if (pos >= 0) {
-          var middlebit = element.splitText(pos),
-              endbit = middlebit.splitText(term.length),
-              middleclone = middlebit.cloneNode(true),
-              spannode = document.createElement('span');
-
-          spannode.className = className;
-          spannode.appendChild(middleclone);
-          middlebit.parentNode.replaceChild(spannode, middlebit);
-          skip = 1;
-        }
-      }
-      else if (element.nodeType == 1 && element.childNodes && !/(script|style)/i.test(element.tagName)) {
-        for (var i = 0; i < element.childNodes.length; ++i)
-          i += innerHighlight(element.childNodes[i], term, className);
-      }
-      return skip;
-    }
-    innerHighlight(element, term, className);
-    return element;
-  },
-  removeHighlight: function(element, term, className) {
-    className = className || 'highlight';
-    $(element).select("span."+className).each(function(e) {
-      e.parentNode.replaceChild(e.firstChild, e);
-    });
-    return element;
-  }
+// Loads functions after DOM is ready
+$(document).ready(function() {
+    addAutofocusBehaviour();
+    addDatePickerBehaviour();
+    addLinkifyContainersBehaviour();
 });
