f0cd2366eff6139af4772b3b0b271b5c2c2b1a99
Roman <roman.simecek@screenconcept.ch>
2010-10-19 11:24:29 +0200

    New simple navigation renderer enabled to load.

--- config/initializers/simple_navigation.rb
+++ config/initializers/simple_navigation.rb
@@ -1,1 +1,3 @@
+require File.join(RAILS_ROOT, 'lib/simple_navigation/renderer/overview')
+
 SimpleNavigation.config_file_path = default_config_file_path = File.join(RAILS_ROOT, 'config', 'navigation')
