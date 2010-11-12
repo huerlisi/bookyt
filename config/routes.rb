654146f81ffe968101ac7b42e1bcd038ce992529
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-12 09:15:50 +0100

    Cleanup comments in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,9 @@
 Bookyt::Application.routes.draw do
+  # Root
+  root :to => "overview#index"
+
+  # Authorization
   devise_for :users
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get :short
-  #       post :toggle
-  #     end
-  #
-  #     collection do
-  #       get :sold
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get :recent, :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-
-  # Authentication
-#  devise_for :users
   
   # Accounting
   resources :account_types
@@ -79,19 +32,8 @@ Bookyt::Application.routes.draw do
   resources :booking_templates
 
   resource :balance
+  match '/profit' => 'profit#show'
   
   # Days
   resources :days
-
-  match '/profit' => 'profit#show'
-  
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => "overview#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  match '/:controller(/:action(/:id))'
 end
