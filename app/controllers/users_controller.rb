a61dfcceda8cf978a97e40fb2d362440c2d8ee7b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-18 14:26:23 +0100

    Only allow updating role_texts if user has ability to manage Role.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < AuthorizedController
       [:password, :password_confirmation, :current_password].collect{|p| params[:user].delete(p) }
     end
 
+    # Test if user is allowed to change roles
+    params[:user].delete(:role_texts) unless can? :manage, Role 
     update!
   end
 end
