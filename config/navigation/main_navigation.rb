15a72e2aa77a6c5ef379310465300425163447a8
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-18 15:48:18 +0100

    Better icon usage for contact and setting navigation items; move company link to setting navigation.

--- config/navigation/main_navigation.rb
+++ config/navigation/main_navigation.rb
@@ -44,27 +44,27 @@ SimpleNavigation::Configuration.run do |navigation|
                      :highlights_on => /\/invoices\/new.*$/
     end
 
-    primary.item :basic_claims_data, t('bookyt.main_navigation.basic_claims_data'), employees_path,
+    primary.item :nav_users, t('bookyt.main_navigation.basic_claims_data'), employees_path,
                  :tooltip => t('bookyt.main_navigation.tooltip.basic_claims_data'),
                  :if => Proc.new { user_signed_in? } do |basic_claims_data|
       basic_claims_data.item :employees, t('bookyt.main_navigation.employees'), employees_path,
                              :tooltip => t('bookyt.main_navigation.tooltip.employees'),
                              :highlights_on => /\/employees$/
-      basic_claims_data.item :employers, t('bookyt.main_navigation.companies'), companies_path,
-                             :tooltip => t('bookyt.main_navigation.tooltip.companies'),
-                             :highlights_on => /\/companies$/
       basic_claims_data.item :customers, t('bookyt.main_navigation.customers'), customers_path,
                              :tooltip => t('bookyt.main_navigation.tooltip.customers'),
                              :highlights_on => /\/customers$/
     end
 
-    primary.item :nav_users, t('bookyt.main_navigation.settings'), current_users_path,
+    primary.item :user_settings, t('bookyt.main_navigation.settings'), current_users_path,
                  :tooltip => t('bookyt.main_navigation.tooltip.settings'),
                  :if => Proc.new { user_signed_in? } do |settings|
       settings.item :current_user, t_title(:current, User), current_users_path
       settings.item :user_index, t_title(:index, User), users_path, :if => lambda { can?(:index, User) }
       settings.item :current_tenant, t_title(:current, Tenant), current_tenants_path, :if => lambda { can?(:current, current_user.tenant) }
       settings.item :tenant_index, t_title(:index, Tenant), tenants_path, :if => lambda { can?(:index, Tenant) }
+      settings.item :employers, t('bookyt.main_navigation.companies'), companies_path,
+                             :tooltip => t('bookyt.main_navigation.tooltip.companies'),
+                             :highlights_on => /\/companies$/
     end
   end
 end
