62427f93dca2222578c618e295aa83b8d01c379c
Ramon Egloff (CyT) <ramon.egloff@cyt.ch>
2010-11-26 08:54:21 +0100

    Updated for sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(:version => 20101123090816) do
 
   create_table "booking_templates", :force => true do |t|
     t.string   "title"
-    t.decimal  "amount",            :precision => 10, :scale => 0
+    t.decimal  "amount"
     t.integer  "credit_account_id"
     t.integer  "debit_account_id"
     t.text     "comments"
@@ -63,30 +63,30 @@ ActiveRecord::Schema.define(:version => 20101123090816) do
 
   create_table "bookings", :force => true do |t|
     t.string  "title",             :limit => 100
-    t.decimal "amount",                           :precision => 10, :scale => 0
+    t.decimal "amount"
     t.integer "credit_account_id"
     t.integer "debit_account_id"
     t.date    "value_date"
-    t.text    "comments"
+    t.text    "comments",          :limit => 1000, :default => ""
     t.string  "scan"
-    t.string  "debit_currency",                                                  :default => "CHF"
-    t.string  "credit_currency",                                                 :default => "CHF"
-    t.float   "exchange_rate",                                                   :default => 1.0
+    t.string  "debit_currency",                    :default => "CHF"
+    t.string  "credit_currency",                   :default => "CHF"
+    t.float   "exchange_rate",                     :default => 1.0
   end
 
   create_table "days", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.date     "date"
-    t.decimal  "cash",            :precision => 10, :scale => 0, :default => 0
-    t.decimal  "card_turnover",   :precision => 10, :scale => 0, :default => 0
-    t.decimal  "gross_turnover",  :precision => 10, :scale => 0, :default => 0
-    t.decimal  "net_turnover",    :precision => 10, :scale => 0, :default => 0
-    t.integer  "client_count",                                   :default => 0
-    t.integer  "product_count",                                  :default => 0
-    t.decimal  "expenses",        :precision => 10, :scale => 0, :default => 0
-    t.decimal  "credit_turnover", :precision => 10, :scale => 0, :default => 0
-    t.decimal  "discount",        :precision => 10, :scale => 0, :default => 0
+    t.decimal  "cash",            :default => 0.0
+    t.decimal  "card_turnover",   :default => 0.0
+    t.decimal  "gross_turnover",  :default => 0.0
+    t.decimal  "net_turnover",    :default => 0.0
+    t.integer  "client_count",    :default => 0
+    t.integer  "product_count",   :default => 0
+    t.decimal  "expenses",        :default => 0.0
+    t.decimal  "credit_turnover", :default => 0.0
+    t.decimal  "discount",        :default => 0.0
   end
 
   create_table "employments", :force => true do |t|
@@ -94,7 +94,7 @@ ActiveRecord::Schema.define(:version => 20101123090816) do
     t.date     "duration_to"
     t.boolean  "temporary"
     t.boolean  "hourly_paid"
-    t.decimal  "daily_workload", :precision => 10, :scale => 0
+    t.decimal  "daily_workload"
     t.integer  "employee_id"
     t.integer  "employer_id"
     t.datetime "created_at"
@@ -119,7 +119,7 @@ ActiveRecord::Schema.define(:version => 20101123090816) do
     t.datetime "updated_at"
     t.string   "title"
     t.text     "remarks"
-    t.decimal  "amount",      :precision => 10, :scale => 0
+    t.decimal  "amount"
   end
 
   create_table "people", :force => true do |t|
