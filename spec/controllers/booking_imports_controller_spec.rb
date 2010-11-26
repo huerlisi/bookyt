c585f5ed523f23f8c21f140e2a363248f56c9291
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-27 00:53:27 +0100

    Adapt booking import controller specs to rename.

--- spec/controllers/booking_imports_controller_spec.rb
+++ spec/controllers/booking_imports_controller_spec.rb
@@ -3,70 +3,70 @@ require 'spec_helper'
 describe BookingImportsController do
   login_admin
 
-  def mock_importer(stubs={})
-    (@mock_importer ||= mock_model(BookingImport).as_null_object).tap do |importer|
-      importer.stub(stubs) unless stubs.empty?
+  def mock_booking_import(stubs={})
+    (@mock_booking_import ||= mock_model(BookingImport).as_null_object).tap do |booking_import|
+      booking_import.stub(stubs) unless stubs.empty?
     end
   end
 
   describe "GET index" do
     it "assigns all booking_imports as @booking_imports" do
-      BookingImport.stub(:all) { [mock_importer] }
+      BookingImport.stub(:all) { [mock_booking_import] }
       get :index
-      assigns(:importers).should eq([mock_importer])
+      assigns(:booking_imports).should eq([mock_booking_import])
     end
   end
 
   describe "GET show" do
-    it "assigns the requested importer as @importer" do
-      BookingImport.stub(:find).with("37") { mock_importer }
+    it "assigns the requested booking_import as @booking_import" do
+      BookingImport.stub(:find).with("37") { mock_booking_import }
       get :show, :id => "37"
-      assigns(:importer).should be(mock_importer)
+      assigns(:booking_import).should be(mock_booking_import)
     end
   end
 
   describe "GET new" do
-    it "assigns a new importer as @importer" do
-      BookingImport.stub(:new) { mock_importer }
+    it "assigns a new booking_import as @booking_import" do
+      BookingImport.stub(:new) { mock_booking_import }
       get :new
-      assigns(:importer).should be(mock_importer)
+      assigns(:booking_import).should be(mock_booking_import)
     end
   end
 
   describe "GET edit" do
-    it "assigns the requested importer as @importer" do
-      BookingImport.stub(:find).with("37") { mock_importer }
+    it "assigns the requested booking_import as @booking_import" do
+      BookingImport.stub(:find).with("37") { mock_booking_import }
       get :edit, :id => "37"
-      assigns(:importer).should be(mock_importer)
+      assigns(:booking_import).should be(mock_booking_import)
     end
   end
 
   describe "POST create" do
 
     describe "with valid params" do
-      it "assigns a newly created importer as @importer" do
-        BookingImport.stub(:new).with({'these' => 'params'}) { mock_importer(:save => true) }
-        post :create, :importer => {'these' => 'params'}
-        assigns(:importer).should be(mock_importer)
+      it "assigns a newly created booking_import as @booking_import" do
+        BookingImport.stub(:new).with({'these' => 'params'}) { mock_booking_import(:save => true) }
+        post :create, :booking_import => {'these' => 'params'}
+        assigns(:booking_import).should be(mock_booking_import)
       end
 
-      it "redirects to the created importer" do
-        BookingImport.stub(:new) { mock_importer(:save => true) }
-        post :create, :importer => {}
-        response.should redirect_to(importer_url(mock_importer))
+      it "redirects to the created booking_import" do
+        BookingImport.stub(:new) { mock_booking_import(:save => true) }
+        post :create, :booking_import => {}
+        response.should redirect_to(booking_import_url(mock_booking_import))
       end
     end
 
     describe "with invalid params" do
-      it "assigns a newly created but unsaved importer as @importer" do
-        BookingImport.stub(:new).with({'these' => 'params'}) { mock_importer(:save => false) }
-        post :create, :importer => {'these' => 'params'}
-        assigns(:importer).should be(mock_importer)
+      it "assigns a newly created but unsaved booking_import as @booking_import" do
+        BookingImport.stub(:new).with({'these' => 'params'}) { mock_booking_import(:save => false) }
+        post :create, :booking_import => {'these' => 'params'}
+        assigns(:booking_import).should be(mock_booking_import)
       end
 
       it "re-renders the 'new' template" do
-        BookingImport.stub(:new) { mock_importer(:save => false) }
-        post :create, :importer => {}
+        BookingImport.stub(:new) { mock_booking_import(:save => false) }
+        post :create, :booking_import => {}
         response.should render_template("new")
       end
     end
@@ -76,34 +76,34 @@ describe BookingImportsController do
   describe "PUT update" do
 
     describe "with valid params" do
-      it "updates the requested importer" do
-        BookingImport.should_receive(:find).with("37") { mock_importer }
-        mock_importer.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, :id => "37", :importer => {'these' => 'params'}
+      it "updates the requested booking_import" do
+        BookingImport.should_receive(:find).with("37") { mock_booking_import }
+        mock_booking_import.should_receive(:update_attributes).with({'these' => 'params'})
+        put :update, :id => "37", :booking_import => {'these' => 'params'}
       end
 
-      it "assigns the requested importer as @importer" do
-        BookingImport.stub(:find) { mock_importer(:update_attributes => true) }
+      it "assigns the requested booking_import as @booking_import" do
+        BookingImport.stub(:find) { mock_booking_import(:update_attributes => true) }
         put :update, :id => "1"
-        assigns(:importer).should be(mock_importer)
+        assigns(:booking_import).should be(mock_booking_import)
       end
 
-      it "redirects to the importer" do
-        BookingImport.stub(:find) { mock_importer(:update_attributes => true) }
+      it "redirects to the booking_import" do
+        BookingImport.stub(:find) { mock_booking_import(:update_attributes => true) }
         put :update, :id => "1"
-        response.should redirect_to(importer_url(mock_importer))
+        response.should redirect_to(booking_import_url(mock_booking_import))
       end
     end
 
     describe "with invalid params" do
-      it "assigns the importer as @importer" do
-        BookingImport.stub(:find) { mock_importer(:update_attributes => false) }
+      it "assigns the booking_import as @booking_import" do
+        BookingImport.stub(:find) { mock_booking_import(:update_attributes => false) }
         put :update, :id => "1"
-        assigns(:importer).should be(mock_importer)
+        assigns(:booking_import).should be(mock_booking_import)
       end
 
       it "re-renders the 'edit' template" do
-        BookingImport.stub(:find) { mock_importer(:update_attributes => false) }
+        BookingImport.stub(:find) { mock_booking_import(:update_attributes => false) }
         put :update, :id => "1"
         response.should render_template("edit")
       end
@@ -112,16 +112,16 @@ describe BookingImportsController do
   end
 
   describe "DELETE destroy" do
-    it "destroys the requested importer" do
-      BookingImport.should_receive(:find).with("37") { mock_importer }
-      mock_importer.should_receive(:destroy)
+    it "destroys the requested booking_import" do
+      BookingImport.should_receive(:find).with("37") { mock_booking_import }
+      mock_booking_import.should_receive(:destroy)
       delete :destroy, :id => "37"
     end
 
-    it "redirects to the booking_imports list" do
-      BookingImport.stub(:find) { mock_importer }
+    it "redirects to the booking_booking_imports list" do
+      BookingImport.stub(:find) { mock_booking_import }
       delete :destroy, :id => "1"
-      response.should redirect_to(importers_url)
+      response.should redirect_to(booking_imports_url)
     end
   end
 
