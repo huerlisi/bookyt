97c7ae433bdd72e7df042a7af3112b3aed19e953
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-27 00:52:06 +0100

    Include devise authentication support for rspec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,4 +24,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  # Devise
+  config.include Devise::TestHelpers, :type => :controller
+  config.extend ControllerMacros, :type => :controller
 end
