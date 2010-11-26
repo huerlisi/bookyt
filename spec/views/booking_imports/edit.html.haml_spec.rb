eb412e952bf7d3fcd6b2c3b45903cda6a319ec53
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-27 00:57:07 +0100

    Use webrat matcher in booking_imports edit view spec.

--- spec/views/booking_imports/edit.html.haml_spec.rb
+++ spec/views/booking_imports/edit.html.haml_spec.rb
@@ -10,8 +10,7 @@ describe "booking_imports/edit.html.haml" do
   it "renders the edit importer form" do
     render
 
-    # Run the generator again with the --webrat-matchers flag if you want to use webrat matchers
-    assert_select "form", :action => booking_import_path(@booking_import), :method => "post" do
+    rendered.should have_selector("form", :action => booking_import_path(@booking_import), :method => "post") do |form|
     end
   end
 end
