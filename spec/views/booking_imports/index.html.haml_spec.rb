6f5b35eeed08e82f5c39ac07e97f5006b93ef409
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-27 01:59:29 +0100

    Ugly fix for booking_imports index view spec.

--- spec/views/booking_imports/index.html.haml_spec.rb
+++ spec/views/booking_imports/index.html.haml_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe "booking_imports/index.html.haml" do
   before(:each) do
     assign(:booking_imports, [
-      stub_model(BookingImport),
-      stub_model(BookingImport)
+      stub_model(BookingImport, :created_at => DateTime.now),
+      stub_model(BookingImport, :created_at => DateTime.now)
     ].paginate(:page => 1))
   end
 
