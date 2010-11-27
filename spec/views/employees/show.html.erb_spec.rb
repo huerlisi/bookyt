ff7ad84bb0718ff4d9c350083d816d62ae163148
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-27 01:32:49 +0100

    Stub resource variable in employees show spec to simulate inherited resources.

--- spec/views/employees/show.html.erb_spec.rb
+++ spec/views/employees/show.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "employees/show.html.erb" do
   before(:each) do
     @employee = assign(:employee, stub_model(Employee))
+    @view.stub(:resource).and_return(@employee)
   end
 
   it "renders attributes in <p>" do
