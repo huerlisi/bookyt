b2d4508bac0c0880ce81d68bd719f90f3fe2fdaf
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-11-30 21:57:55 +0100

    Move from black- to whitelist in attribute copying for template bookings.

--- app/models/booking_template.rb
+++ app/models/booking_template.rb
@@ -24,7 +24,7 @@ class BookingTemplate < ActiveRecord::Base
   end
 
   def build_booking(params = {})
-    booking_params = attributes.reject{|key, value| ["updated_at", "created_at", "id", "code"].include?(key)}
+    booking_params = attributes.reject{|key, value| !["title", "amount", "comments", "credit_account_id", "debit_account_id"].include?(key)}
     booking_params.merge!(params)
 
     Booking.new(booking_params)
