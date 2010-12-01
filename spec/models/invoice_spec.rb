dbaf7b0aed57636e192e2f706a001a188d7c9f01
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-12-01 22:50:29 +0100

    Refactor invoice spec.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,22 +1,31 @@
 require 'spec_helper'
 
 describe Invoice do
+  it { should belong_to(:customer) }
+  it { should belong_to(:company) }
+
+  it { should validate_presence_of(:customer) }
+  it { should validate_presence_of(:company) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:amount) }
+  it { should validate_presence_of(:state) }
+  
   context "when new" do
     specify { should_not be_valid }
+
     its(:to_s) { should == "" }
   end
 
-  context "when valid" do
-    subject { Factory.create :invoice }
-    specify { should be_valid }
-    
-    it "should loose validity without customer" do
-      subject.customer = nil
-      subject.should_not be_valid
-    end
-    it "should loose validity without company" do
-      subject.company = nil
-      subject.should_not be_valid
-    end
+  context "when amount is nil" do
+    before(:all) { subject.amount = nil }
+
+    its(:to_s) { should == "" }
+  end
+  
+  context "when properly initialized" do
+    subject { Factory.build :invoice }
+
+    its(:to_s) { should =~ /New Invoice/ }
+    its(:to_s) { should =~ /99.85/ }
   end
 end
