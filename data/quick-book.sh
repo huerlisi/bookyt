6bcf3b8e67d3b6573ff0157467d0cb72b45cc29d
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-01-05 14:38:55 +0100

    Fix quick-book.

--- data/quick-book.sh
+++ data/quick-book.sh
@@ -57,14 +57,16 @@ local debit_account="$2"
 }
 
 
-cat $file | add_value_date | parse_chf_ebanking 'E-Banking' | csv2booking 41 2 | /srv/agrabah.ch/accounting/script/console production
-cat $file | add_value_date | add_title "Habenzins" | csv2booking 2 38 | /srv/agrabah.ch/accounting/script/console production
-cat $file | add_value_date | add_title "Verrechnungssteuer" | csv2booking 50 2 | /srv/agrabah.ch/accounting/script/console production
-cat $file | add_value_date | add_title "Kontoführungsgebühren" | csv2booking 39 2 | /srv/agrabah.ch/accounting/script/console production
-cat $file | add_value_date | add_title "Versandspesen" | csv2booking 39 2 | /srv/agrabah.ch/accounting/script/console production
-cat $file | add_value_date | add_chf_value_eur_ebanking | csv2booking 40 2 | /srv/agrabah.ch/accounting/script/console production
-cat $file | add_value_date | comment_as_title 'Barauszahlung' | csv2booking 1 2 | /srv/agrabah.ch/accounting/script/console production
-cat $file | add_value_date | add_title 'Bancomatbezug' | csv2booking 1 2 | /srv/agrabah.ch/accounting/script/console production
-cat $file | add_value_date | add_title 'Bareinzahlung' | csv2booking 2 1 | /srv/agrabah.ch/accounting/script/console production
-cat $file | add_value_date | add_value_and_title | grep 'Aduno' | csv2booking 2 49 | /srv/agrabah.ch/accounting/script/console production
-cat $file | add_value_date | add_value_and_title | grep 'Gutschrift' | grep -v 'Aduno' | csv2booking 2 52 | /srv/agrabah.ch/accounting/script/console production
+cat $file | add_value_date | parse_chf_ebanking 'E-Banking' | csv2booking 40 2
+cat $file | add_value_date | add_title "Habenzins" | csv2booking 2 38
+cat $file | add_value_date | add_title "Verrechnungssteuer" | csv2booking 50 2
+cat $file | add_value_date | add_title "Kontoführungsgebühren" | csv2booking 39 2
+cat $file | add_value_date | add_title "Gebühren" | csv2booking 39 2
+cat $file | add_value_date | add_title "gebühr" | csv2booking 39 2
+cat $file | add_value_date | add_title "Versandspesen" | csv2booking 39 2
+cat $file | add_value_date | add_chf_value_eur_ebanking | csv2booking 40 2
+cat $file | add_value_date | comment_as_title 'Barauszahlung' | csv2booking 1 2
+cat $file | add_value_date | add_title 'Bancomatbezug' | csv2booking 1 2
+cat $file | add_value_date | add_title 'Bareinzahlung' | csv2booking 2 1
+cat $file | add_value_date | add_value_and_title | grep 'Aduno' | csv2booking 2 49
+cat $file | add_value_date | add_value_and_title | grep 'Gutschrift' | grep -v 'Aduno' | csv2booking 2 52
