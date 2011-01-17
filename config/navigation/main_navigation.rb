e7f9d8f2fc32b6724c258691edf5b5dfe0a08aa5
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-01-17 11:56:35 +0100

    Fix path for day list navigation item.

--- config/navigation/main_navigation.rb
+++ config/navigation/main_navigation.rb
@@ -9,7 +9,7 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item :store, t('bookyt.main_navigation.store'), new_day_path,
                  :tooltip => t('bookyt.main_navigation.tooltip.store'),
                  :if => Proc.new { user_signed_in? } do |store|
-      store.item :day, t('bookyt.main_navigation.days'), new_day_path,
+      store.item :day, t('bookyt.main_navigation.days'), days_path,
                  :tooltip => t('bookyt.main_navigation.tooltip.days'),
                  :highlights_on => /\/days($|\?)/
       store.item :day, t('bookyt.main_navigation.new_day'), new_day_path,
