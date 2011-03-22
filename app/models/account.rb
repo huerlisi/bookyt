62fb83e1dbdabfc75bfb1209dfd3956e79fcc6e9
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-03-22 16:35:02 +0100

    Fix indentation in Account.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,13 +1,13 @@
 module AccountScopeExtension
-    def saldo(selector = Date.today, inclusive = true)
-      new_saldo = 0
+  def saldo(selector = Date.today, inclusive = true)
+    new_saldo = 0
 
-      for account in all
-        new_saldo += account.saldo(selector, inclusive)
-      end
-
-      return new_saldo
+    for account in all
+      new_saldo += account.saldo(selector, inclusive)
     end
+
+    return new_saldo
+  end
 end
 
 class Account < ActiveRecord::Base
