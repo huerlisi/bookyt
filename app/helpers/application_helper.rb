0dfe24d7c9fc147aad6968465f76603aa497309d
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-03-22 17:04:51 +0100

    Fix currency_fmt helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
 
   def currency_fmt(value)
     # We often do get -0.0 but don't like it
-    value = 0.0 if value.to_s.match %r{-[0]*[.0]*}
+    value = 0.0 if value.to_s.match %r{^-[0.]*$}
     
     number = number_with_precision(value, :precision => 2, :separator => '.', :delimiter => "'")
   end
