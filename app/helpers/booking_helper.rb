7be821670c52ec04a8b17cfb398d90531935c153
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-03-23 11:00:51 +0100

    Implement basic reference association for bookings.

---
+++ app/helpers/booking_helper.rb
@@ -0,0 +1,9 @@
+module BookingHelper
+  def reference_types_as_collection
+    types = [CreditInvoice, DebitInvoice]
+    types.inject({}) do |result, type|
+      result[t_model(type)] = type.base_class
+      result
+    end
+  end
+end
