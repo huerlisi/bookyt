0f9e90c07f34faf37db0f3c4dd376e2b1e1232bb
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-03-31 22:13:23 +0200

    Preseed value and due dates for new invoices.

--- app/controllers/credit_invoices_controller.rb
+++ app/controllers/credit_invoices_controller.rb
@@ -2,7 +2,12 @@ class CreditInvoicesController < AuthorizedController
   # Actions
   def new
     # Allow pre-seeding some parameters
-    invoice_params = {:customer_id => current_tenant.company.id, :state => 'booked'}
+    invoice_params = {
+      :customer_id => current_tenant.company.id,
+      :state       => 'booked',
+      :value_date  => Date.today,
+      :due_date    => Date.today.in(30.days)
+    }
 
     # Set default parameters
     invoice_params.merge!(params[:invoice]) if params[:invoice]
--- app/controllers/debit_invoices_controller.rb
+++ app/controllers/debit_invoices_controller.rb
@@ -2,7 +2,12 @@ class DebitInvoicesController < AuthorizedController
   # Actions
   def new
     # Allow pre-seeding some parameters
-    invoice_params = {:company_id => current_tenant.company.id, :state => 'booked'}
+    invoice_params = {
+      :company_id => current_tenant.company.id,
+      :state      => 'booked',
+      :value_date => Date.today,
+      :due_date   => Date.today.in(30.days).to_date
+    }
 
     # Set default parameters
     invoice_params.merge!(params[:invoice]) if params[:invoice]
