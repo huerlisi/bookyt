92a3f37298b9bcb4d4bfce9dd37f7c2b13c48f17
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-04 11:09:45 +0200

    Use .to_date in pre seeded due_date on credit_invoices.

--- app/controllers/credit_invoices_controller.rb
+++ app/controllers/credit_invoices_controller.rb
@@ -6,7 +6,7 @@ class CreditInvoicesController < AuthorizedController
       :customer_id => current_tenant.company.id,
       :state       => 'booked',
       :value_date  => Date.today,
-      :due_date    => Date.today.in(30.days)
+      :due_date    => Date.today.in(30.days).to_date
     }
 
     # Set default parameters
