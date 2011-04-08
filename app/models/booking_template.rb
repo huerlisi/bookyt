20de18ab2826bf8ec78c99dc27db8f250f0e1ffe
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-08 15:17:32 +0200

    Code refactoring for robustness in booking template amount calculation.

--- app/models/booking_template.rb
+++ app/models/booking_template.rb
@@ -31,21 +31,20 @@ class BookingTemplate < ActiveRecord::Base
     booking_params = attributes.reject!{|key, value| !["title", "comments", "credit_account_id", "debit_account_id"].include?(key)}
 
     # Calculate amount
+    booking_amount = BigDecimal.new(attributes['amount'] || '0')
+
     if ref_type = params['reference_type'] and ref_id = params['reference_id']
       reference = ref_type.constantize.find(ref_id)
 
       case self.amount_relates_to
         when 'reference_amount'
-          booking_params['amount'] = reference.amount * BigDecimal.new(attributes['amount'])
+          booking_amount *= reference.amount
         when 'reference_balance'
-          booking_params['amount'] = reference.balance * BigDecimal.new(attributes['amount'])
-        else
-          booking_params['amount'] = BigDecimal.new(attributes['amount'] || '0.0')
+          booking_amount *= reference.balance
       end
-      
-    else
-      booking_params['amount'] = BigDecimal.new(attributes['amount'])
     end
+
+    booking_params['amount'] = booking_amount
     
     # Override by passed in parameters
     booking_params.merge!(params)
