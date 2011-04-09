1883ce6bee3fd1caa2ad3f06187f60280a952087
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-09 13:44:38 +0200

    Fix customer and company view.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,5 @@
 class CustomersController < AuthorizedController
   def show
-    @employments = resource.employments.paginate(:page => params[:page])
-
     @credit_invoices = resource.credit_invoices.paginate(:page => params[:page])
     @credit_invoices_amount = resource.credit_invoices.to_a.sum(&:amount)
     @credit_invoices_balance = resource.credit_invoices.to_a.sum(&:balance)
