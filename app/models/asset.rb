da277ea7debb5b2c5aa29ccfcc102319428c32cb
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-10 22:08:07 +0200

    Try to guess direct account for Asset.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -8,4 +8,17 @@ class Asset < ActiveRecord::Base
   # Bookings
   # ========
   include HasAccounts::Model
+  
+  def direct_account
+    # We don't care if no bookings
+    return nil if bookings.empty?
+    
+    # Take any booking
+    booking = bookings.first
+    involved_accounts = [booking.credit_account, booking.debit_account]
+    
+    relevant_account = involved_accounts - [Account.find_by_code("2000")]
+    
+    return relevant_account.first
+  end
 end
