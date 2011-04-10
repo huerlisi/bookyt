1365920b8b9ab1d085687d13bd4e5533ae63ec64
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-10 21:59:47 +0200

    Validate presence of title, amount, state in Assets.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,6 +2,9 @@ class Asset < ActiveRecord::Base
   # Invoice
   belongs_to :invoice
 
+  # Validations
+  validates_presence_of :title, :amount, :state
+
   # Bookings
   # ========
   include HasAccounts::Model
