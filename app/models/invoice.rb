7de5db6f89a67e08ea9c178112638b8b077e0168
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-10 13:29:38 +0200

    Use HasAccounts::Model concern for Invoice.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -22,46 +22,7 @@ class Invoice < ActiveRecord::Base
   
   # Bookings
   # ========
-  def direct_account
-    nil
-  end
-  
-  def direct_account
-    self.class.direct_account
-  end
-
-  has_many :bookings, :as => :reference, :dependent => :destroy do
-    # TODO: duplicated in Booking (without parameter)
-    def direct_balance(value_date = nil, direct_account = nil)
-      return BigDecimal.new('0') unless proxy_owner.direct_account
-      
-      direct_account ||= proxy_owner.direct_account
-      balance = BigDecimal.new('0')
-
-      direct_bookings = scoped
-      direct_bookings = direct_bookings.where("value_date <= ?", value_date) if value_date
-
-      for booking in direct_bookings.all
-        balance += booking.accounted_amount(direct_account)
-      end
-
-      balance
-    end
-  end
-
-  def build_booking
-    booking_template = BookingTemplate.find_by_code(self.class.to_s.underscore + ':invoice')
-    
-    booking = booking_template.build_booking(:value_date => value_date, :amount => amount)
-    bookings << booking
-    
-    booking
-  end
-
-  # TODO: called due_amount in CyDoc
-  def balance(value_date = nil)
-    bookings.direct_balance(value_date)
-  end
+  include HasAccounts::Model
   
   # Helpers
   def to_s
