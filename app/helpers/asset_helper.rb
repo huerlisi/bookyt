b1e74e4388c52edf1ba6771e39b90ed09086b340
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-11 21:13:30 +0200

    Add t_asset_state helper, use it.

--- app/helpers/asset_helper.rb
+++ app/helpers/asset_helper.rb
@@ -1,8 +1,14 @@
 module AssetHelper
+  # Translate asset state
+  def t_asset_state(state)
+    t(state, :scope => 'asset.state')
+  end
+  
+  # Provide translated asset states for select fields
   def asset_states_as_collection
     states = Asset::STATES
     states.inject({}) do |result, state|
-      result[t(state, :scope => 'asset.state')] = state
+      result[t_asset_state(state)] = state
       result
     end
   end
