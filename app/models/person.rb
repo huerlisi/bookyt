06e5598c944b52863996ed02c4717e44370ec5f1
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-12 11:18:03 +0200

    Order person by name.

--- app/models/person.rb
+++ app/models/person.rb
@@ -24,6 +24,8 @@ class Person < ActiveRecord::Base
   accepts_nested_attributes_for :vcard
 
   # Search
+  default_scope includes(:vcard).order('IFNULL(vcards.full_name, vcards.family_name + ' ' + vcards.given_name)')
+  
   scope :by_name, lambda {|value|
     includes(:vcard).where("(vcards.given_name LIKE :query) OR (vcards.family_name LIKE :query) OR (vcards.full_name LIKE :query)", :query => "%#{value}%")
   }
