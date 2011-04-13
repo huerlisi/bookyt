e695c0d2c768b9328297a40ac2eb8441281576d9
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-13 09:15:08 +0200

    Some usability fixes for new asset form in invoice view.

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -4,6 +4,28 @@ class AssetsController < AuthorizedController
   has_scope :by_text
   
   # Actions
+  def new
+
+    # Defaults
+    asset_params = {
+      :state  => 'available'
+    }
+
+    # Load and assign parent invoice
+    invoice = Invoice.find(params[:invoice_id])
+    if invoice
+      asset_params.merge!(
+        :title  => invoice.title,
+        :amount => invoice.amount
+      )
+    end
+    
+    # Paramameters
+    asset_params.merge!(params[:asset])
+
+    @asset = Asset.new(asset_params)
+  end
+  
   def create
     @asset = Asset.new(params[:asset])
     @asset.build_booking
