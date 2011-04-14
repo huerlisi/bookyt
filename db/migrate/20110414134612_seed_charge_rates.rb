1cee65804af8a12d81c0877f41a11dfd4c6c26f7
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-14 15:47:23 +0200

    Seed charge rates.

---
+++ db/migrate/20110414134612_seed_charge_rates.rb
@@ -0,0 +1,17 @@
+class SeedChargeRates < ActiveRecord::Migration
+  def self.up
+    # Salaries
+    # ========
+    # Charge Rates
+    ChargeRate.create!([
+      {:code => 'salary:both:ahv_iv_eo', :title => "AHV/IV/EO", :duration_from => '2007-01-01', :rate => 5.05},
+      {:code => 'salary:both:ahv_iv_eo', :title => "AHV/IV/EO", :duration_from => '2011-01-01', :rate => 5.15},
+      {:code => 'salary:both:alv', :title => "ALV", :duration_from => '2007-01-01', :rate => 1},
+      {:code => 'salary:both:alv', :title => "ALV", :duration_from => '2011-01-01', :rate => 1.1},
+      {:code => 'salary:both:alv_solidarity', :title => "ALV Solidaritätsprozent", :duration_from => '2011-01-01', :rate => 1},
+    ])
+  end
+
+  def self.down
+  end
+end
