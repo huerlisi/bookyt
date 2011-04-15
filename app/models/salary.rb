8a5bcf7ce5f78b1861e25c519efe9f61073e8fc9
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-15 11:28:45 +0200

    Include net_amount in salary list.

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -1,4 +1,11 @@
 class Salary < Invoice
+  def net_amount
+    salary_invoice_booking = bookings.where(:debit_account_id => Account.find_by_code('2050').id).first
+    return 0.0 unless salary_invoice_booking
+
+    salary_invoice_booking.amount
+  end
+  
   # Bookings
   def self.direct_account
     Account.find_by_code("2050")
