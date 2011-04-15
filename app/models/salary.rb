13598c44b25249d8c6d535bdae9953231f06b1b7
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-15 13:29:01 +0200

    Create bookings for salary.

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -8,7 +8,26 @@ class Salary < Invoice
   end
   
   # Bookings
+  # ========
   def self.direct_account
-    Account.find_by_code("2050")
+    Account.find_by_code("5000")
+  end
+
+  # Build booking
+  #
+  # We use the value_date of the purchase invoice but our own amount.
+  def build_booking(params = {}, template_code = nil)
+    # Build and assign booking
+    super(params, 'salary:employee:ahv_iv_eo').save
+    super(params, 'salary:employer:ahv_iv_eo').save
+    super(params, 'salary:employee:alv').save
+    super(params, 'salary:employer:alv').save
+    super(params, 'salary:employee:nbu').save
+    super(params, 'salary:employer:nbu').save
+    super(params, 'salary:employer:bu').save
+    super(params, 'salary:employer:fak').save
+    super(params, 'salary:employer:vkb').save
+    
+    super(params, 'salary:invoice').save
   end
 end
