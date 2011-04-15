d4d0bed3c07c97b4fc9a72aabb6cd4a1ae9253bb
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-15 13:45:43 +0200

    Fix amount_relates_to migration.

--- db/migrate/20110415113924_salary_should_use_relative_amount.rb
+++ db/migrate/20110415113924_salary_should_use_relative_amount.rb
@@ -1,7 +1,7 @@
 class SalaryShouldUseRelativeAmount < ActiveRecord::Migration
   def self.up
     salary_invoice = BookingTemplate.find_by_code('salary:invoice')
-    salary_invoice.relates_to = 'reference_amount_minus_balance'
+    salary_invoice.amount_relates_to = 'reference_amount_minus_balance'
     salary_invoice.amount = 1
     salary_invoice.save
   end
