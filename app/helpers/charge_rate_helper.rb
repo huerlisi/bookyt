02084b9c94049bf8eee8b604969adc4f811fbcd8
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-18 14:06:18 +0200

    Include person_id in ChargeRate.latest and codes helper.

--- app/helpers/charge_rate_helper.rb
+++ app/helpers/charge_rate_helper.rb
@@ -1,5 +1,5 @@
 module ChargeRateHelper
   def codes_as_collection
-    ChargeRate.latest.map{|charge_rate| [charge_rate.title, charge_rate.code]}
+    ChargeRate.latest.map{|charge_rate| ["%s (%s)" % [charge_rate.title, charge_rate.person], charge_rate.code]}
   end
 end
