8c41e89a232e952cc7e58a57de37417a956cdb32
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-20 12:42:53 +0200

    Support attachments for employments.

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -7,8 +7,13 @@ class Employment < ActiveRecord::Base
   validates_presence_of :employee, :employer
   validates_date :duration_from, :duration_to, :allow_nil => true, :allow_blank => true
 
-  # Helpers
+  # String
   def to_s
     "%s bei %s von %s - %s" % [employee, employer, duration_from, duration_to]
   end
+
+  # Attachments
+  # ===========
+  has_many :attachments, :as => :object
+  accepts_nested_attributes_for :attachments
 end
