424055438a30796161ccfe977bb4b0a39948e40c
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-20 12:08:44 +0200

    Add employments navigation.

--- config/navigation/main_navigation.rb
+++ config/navigation/main_navigation.rb
@@ -101,6 +101,7 @@ SimpleNavigation::Configuration.run do |navigation|
       salaries.item :salaries, t_title(:index, Salary), salaries_path, :highlights_on => /\/salaries($|\/[0-9]*($|\/.*))/
       salaries.item :new_salary, t_title(:new, Salary), new_salary_path
       salaries.item :employees, t_title(:index, Employee), employees_path
+      salaries.item :employments, t_title(:index, Employment), employments_path, :highlights_on => /\/employments($|\/[0-9]*($|\/.*))/
       salaries.item :salary_statistics, t_title(:statistics, Salary), statistics_salaries_path, :highlights_on => /\/salaries\/statistics($|\?)/
       account_item(salaries, '5000')
     end
