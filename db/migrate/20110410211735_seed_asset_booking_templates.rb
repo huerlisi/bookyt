3c60e6800658563d6c38a3819de071fa5762586b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-23 12:46:14 +0200

    Add column information reload to fix migration path.

--- db/migrate/20110410211735_seed_asset_booking_templates.rb
+++ db/migrate/20110410211735_seed_asset_booking_templates.rb
@@ -1,5 +1,8 @@
 class SeedAssetBookingTemplates < ActiveRecord::Migration
   def self.up
+    # Workaround as we changed BookingTemplate before
+    BookingTemplate.reset_column_information
+
     BookingTemplate.create!([
       {:code => "asset:activate", :title => "Aktivierung", :debit_account => Account.find_by_code("4000"), :credit_account => Account.find_by_code("1230"), :amount => 1, :amount_relates_to => 'reference_amount'},
 
