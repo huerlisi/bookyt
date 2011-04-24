4119cce678d264f2a261cb248db3379c86643be1
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-04-24 07:23:23 +0200

    Some sqlite cap fixes for multistage.

--- lib/recipes/sqlite.rb
+++ lib/recipes/sqlite.rb
@@ -1,21 +1,27 @@
+before "deploy:setup", "sqlite:setup"
 after "db:symlink", "sqlite:symlink"
 
 namespace :sqlite do
+  desc "Setup Sqlite3 db"
+  task :setup do
+    run "mkdir -p #{shared_path}/db"
+  end
+  
   namespace :sync do
     desc "Sync down the production sqlite database to local development sqlite"
     task :down do
-      download "#{deploy_to}/shared/db/production.sqlite3", "db/development.sqlite3", :via => :scp
+      download "#{shared_path}/db/production.sqlite3", "db/development.sqlite3", :via => :scp
     end
 
     desc "Sync up the development sqlite database to production sqlite"
     task :up do
       run "mkdir -p #{shared_path}/db"
-      upload "db/development.sqlite3", "#{deploy_to}/shared/db/production.sqlite3", :via => :scp
+      upload "db/development.sqlite3", "#{shared_path}/db/production.sqlite3", :via => :scp
     end
   end
 
   desc "Make symlink for shared database"
   task :symlink do
-    run "ln -nfs #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
+    run "ln -nfs #{shared_path}/db/#{rails_env}.sqlite3 #{release_path}/db/#{rails_env}.sqlite3"
   end
 end
