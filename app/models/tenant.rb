ee8b6e98d290b95972410aab20e704387c0d7d55
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-05-02 12:46:25 +0200

    New method Tenant.fiscal_period(year).

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -16,15 +16,21 @@ class Tenant < ActiveRecord::Base
   # Fiscal Years
   # ============
   validates_date :fiscal_year_ends_on
+
+  def fiscal_period(year)
+    final_day_of_fiscal_year = Date.new(year, fiscal_year_ends_on.month, fiscal_year_ends_on.day)
+    first_day_of_fiscal_year = final_day_of_fiscal_year.ago(1.year).in(1.day)
+
+    return :from => first_day_of_fiscal_year.to_date, :to => final_day_of_fiscal_year.to_date
+  end
+  
   def fiscal_years
     first_year = fiscal_year_ends_on.year
     final_year = Date.today.year + 1
     
     years = {}
     (first_year..final_year).map{|year|
-      final_day_of_fiscal_year = Date.new(year, fiscal_year_ends_on.month, fiscal_year_ends_on.day)
-      first_day_of_fiscal_year = final_day_of_fiscal_year.ago(1.year).in(1.day)
-      years[year] = {:from => first_day_of_fiscal_year.to_date, :to => final_day_of_fiscal_year.to_date }
+      years[year] = fiscal_period(year)
     }
     
     return years
