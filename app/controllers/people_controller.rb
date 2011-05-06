5fde4b826e563f7472453be258f2303cea66f8f2
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-05-06 11:07:25 +0200

    Fix invoice balance calculations; support filtering.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,6 +3,11 @@ class PeopleController < AuthorizedController
     @credit_invoices = resource.credit_invoices
     @debit_invoices = resource.debit_invoices
     
+    # Invoice scoping by state
+    by_state = params[:by_state] || 'booked'
+    @credit_invoices = @credit_invoices.by_state(by_state)
+    @debit_invoices = @debit_invoices.by_state(by_state)
+
     show!
   end
 end
