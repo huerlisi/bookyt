d350a792b888462893973f0d7aa28a31132fc7f8
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-05-08 12:26:24 +0200

    Try adding .includes to prefetch in account views.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,9 +3,13 @@ class AccountsController < AuthorizedController
   has_scope :by_value_period, :using => [:from, :to], :default => proc { |c| c.session[:has_scope] }
   has_scope :by_text
 
+  def index
+    @accounts = apply_scopes(Account).includes(:account_type).includes(:credit_bookings, :credit_bookings).paginate(:page => params['page'])
+  end
+  
   def show
     @account = Account.find(params[:id])
-    @bookings = apply_scopes(Booking).by_account(@account)
+    @bookings = apply_scopes(Booking).includes(:debit_account => :account_type, :credit_account => :account_type).by_account(@account)
     
     if params[:only_credit_bookings]
       @bookings = @bookings.where(:credit_account_id => @account.id)
