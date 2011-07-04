d6e0e3fa49a7a36fe2d17222f84dcf311bd5f19b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-07-04 22:20:27 +0200

    Reduce compass config to what we really need.

--- config/compass.rb
+++ config/compass.rb
@@ -1,16 +1,20 @@
 # This configuration file works with both the Compass command line tool and within Rails.
 # Require any additional compass plugins here.
 project_type = :rails
-project_path = Compass::AppIntegration::Rails.root
+
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "public/stylesheets/compiled"
-http_stylesheets_path = "/stylesheets"
-sass_dir = "app/stylesheets"
-images_dir = "public/images"
-http_images_path = "/images"
-javascripts_dir = "public/javascripts"
-http_javascripts_path = "/javascripts"
-environment = Compass::AppIntegration::Rails.env
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+# Use compiled for CSS output
+css_dir = "public/stylesheets/compiled"
+
+preferred_syntax = :sass
