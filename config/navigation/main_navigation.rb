50dd75b3150118347dcd6fd0d30f2d01c35629a8
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-08-03 16:04:45 +0200

    Drop navigation for salary engine.

--- config/navigation/main_navigation.rb
+++ config/navigation/main_navigation.rb
@@ -92,16 +92,6 @@ SimpleNavigation::Configuration.run do |navigation|
       assets.item :write_downs, t_title(:write_downs, Asset), write_downs_assets_path, :highlights_on => /\/assets\/write_downs($|\?)/
     end
 
-    primary.item :salaries, t_title(:index, Salary), salaries_path,
-                 :if => Proc.new { user_signed_in? } do |salaries|
-      salaries.item :salaries, t_title(:index, Salary), salaries_path, :highlights_on => /\/salaries($|\/[0-9]*($|\/.*))/
-      salaries.item :new_salary, t_title(:new, Salary), new_salary_path
-      salaries.item :employees, t_title(:index, Employee), employees_path
-      salaries.item :employments, t_title(:index, Employment), employments_path, :highlights_on => /\/employments($|\/[0-9]*($|\/.*))/
-      salaries.item :salary_statistics, t_title(:statistics, Salary), statistics_salaries_path, :highlights_on => /\/salaries\/statistics($|\?)/
-      account_item(salaries, '5000')
-    end
-
     primary.item :settings, t('bookyt.main_navigation.settings'), current_users_path,
                  :tooltip => t('bookyt.main_navigation.tooltip.settings'),
                  :if => Proc.new { user_signed_in? } do |settings|
