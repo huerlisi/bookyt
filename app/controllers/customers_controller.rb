c2e7dfbcf58767759d22bed1e6657525fe8bdfff
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-08-08 00:26:44 +0200

    Use fact that Customer has debit invoices.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,9 @@
 class CustomersController < PeopleController
+  def show
+    # Invoice scoping by state
+    by_state = params[:by_state] || 'all'
+    @debit_invoices = resource.invoices.reorder('due_date DESC').by_state(by_state)
+
+    show!
+  end
 end
