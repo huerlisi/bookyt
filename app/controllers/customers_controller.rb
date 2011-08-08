0c83d9e27aeff342e8b96618edb38c75df718f57
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-08-08 13:49:12 +0200

    Show open and paid invoice lists in customer view.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,8 @@ class CustomersController < PeopleController
   def show
     # Invoice scoping by state
     by_state = params[:by_state] || 'all'
-    @debit_invoices = resource.invoices.reorder('due_date DESC').by_state(by_state)
+    @open_debit_invoices = resource.invoices.reorder('due_date DESC').by_state('booked')
+    @paid_debit_invoices = resource.invoices.reorder('due_date DESC').by_state(['canceled', 'paid'])
 
     show!
   end
