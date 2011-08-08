8b58d373bb31546b184e3a43618c1624e05e8fff
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-08-08 11:25:58 +0200

    Drop Asset from list of Booking reference models.

--- app/helpers/booking_helper.rb
+++ app/helpers/booking_helper.rb
@@ -1,6 +1,7 @@
 module BookingHelper
   def reference_types_as_collection
-    types = [Invoice, Asset]
+    # TODO: Stock model should be available too, from bookyt_stock
+    types = [Invoice]
     types.inject({}) do |result, type|
       result[t_model(type)] = type.base_class
       result
