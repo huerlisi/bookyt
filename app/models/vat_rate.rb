0f06b076015874bb514d6a1589c1534972c96a3e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-09-15 22:02:58 +0200

    Add VatRate.to_s returning rate and translated code.

--- app/models/vat_rate.rb
+++ app/models/vat_rate.rb
@@ -3,4 +3,9 @@ class VatRate < ChargeRate
   scope :reduced, where(:code => 'reduced')
   scope :special, where(:code => 'special')
   scope :excluded, where(:code => 'excluded')
+
+  # String
+  def to_s
+    "%.1f (%s)" % [rate, I18n::translate(code, :scope => 'vat_rates.code')]
+  end
 end
