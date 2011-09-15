311d6f30524f4b23b935b4541aea8d97282e7015
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-09-15 22:04:34 +0200

    Drop vat: prefix in VatRate seeds.

--- db/seeds/locales/de-CH.rb
+++ db/seeds/locales/de-CH.rb
@@ -4,8 +4,8 @@
 
 # Vat Rates
 VatRate.create!([
-  {:code => 'vat:full', :title => "MWSt. (Normaler Satz)", :duration_from => '2011-01-01', :rate => 8},
-  {:code => 'vat:reduced', :title => "MWSt. (Reduzierter Satz)", :duration_from => '2011-01-01', :rate => 2.5},
-  {:code => 'vat:special', :title => "MWSt. (Sondersatz)", :duration_from => '2011-01-01', :rate => 2.5},
-  {:code => 'vat:excluded', :title => "MWSt. befreit", :duration_from => '2011-01-01', :rate => 0},
+  {:code => 'full', :title => "MWSt. (Normaler Satz)", :duration_from => '2011-01-01', :rate => 8},
+  {:code => 'reduced', :title => "MWSt. (Reduzierter Satz)", :duration_from => '2011-01-01', :rate => 2.5},
+  {:code => 'special', :title => "MWSt. (Sondersatz)", :duration_from => '2011-01-01', :rate => 2.5},
+  {:code => 'excluded', :title => "MWSt. befreit", :duration_from => '2011-01-01', :rate => 0},
 ])
