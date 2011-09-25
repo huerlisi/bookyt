7b96ecbb3564084a90e1aa5e1d31f92b6e1c69b6
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-09-25 14:25:06 +0200

    Drop explicit compass and sass-rails require (workaround for asset pipeline bug).

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-require 'compass'
-require 'sass-rails'
-
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require *Rails.groups(:assets) if defined?(Bundler)
