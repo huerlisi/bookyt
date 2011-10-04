e0995b9ed0ac5973256a1e9e12d805dc90448695
Roman Simecek <roman.simecek@cyt.ch>
2011-10-04 15:41:59 +0200

    Seed extended with civil status and religion.

--- db/seeds/locales/de-CH.rb
+++ db/seeds/locales/de-CH.rb
@@ -9,3 +9,19 @@ VatRate.create!([
   {:code => 'special', :title => "MWSt. (Sondersatz)", :duration_from => '2011-01-01', :rate => 2.5},
   {:code => 'excluded', :title => "MWSt. befreit", :duration_from => '2011-01-01', :rate => 0},
 ])
+
+CivilStatus.create!([
+  {:name => "Ledig"}, 
+  {:name => "Verheiratet"},
+  {:name => "Geschieden"}, 
+  {:name => "Verwitwet"}
+])
+
+Religion.create!([
+  {:name => "Römisch-Katholisch"},
+  {:name => "Reformiert"},
+  {:name => "Orthodox"},
+  {:name => "Islam"},
+  {:name => "Jüdisch"},
+  {:name => "Konfessionslos"}
+])
