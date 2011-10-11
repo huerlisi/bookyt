7b291d5a3160bfb8cee037986ec9e3c1c4fca7bb
Roman Simecek <roman.simecek@cyt.ch>
2011-10-11 09:17:49 +0200

    Association fixed.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,23 @@
 FactoryGirl.define do
+  factory :user_role, :class => Role do
+    name 'user'
+  end
+
+  factory :admin_role, :class => Role do
+    name 'admin'
+  end
+
   factory :user do
     email    "user@example.com"
     password "user1234"
-    roles [FactoryGirl.create(:role, :name => 'user')]
+    association :user_role
     person
   end
 
   factory :admin_user, :class => User do
     email    "admin@example.com"
     password "admin1234"
-    roles [FactoryGirl.create(:role, :name => 'admin')]
+    association :admin_role
     person
   end
 end
