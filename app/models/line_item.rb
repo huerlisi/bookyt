27c2bcfd4c0da74c718365cd9391e7360b1b4e1b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-03 12:37:57 +0100

    Factor out LineItem.times_to_s from LetterDocument prawn template.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,8 @@
 class LineItem < ActiveRecord::Base
+  # Aspects
+  include ApplicationHelper
+
+  # Associations
   belongs_to :item, :polymorphic => true
   belongs_to :invoice, :touch => true
 
@@ -18,5 +22,19 @@ class LineItem < ActiveRecord::Base
 
   def total_amount
     times * price.to_f
+  end
+
+  def times_to_s
+    if times == 1
+      if quantity == "x"
+        s = ""
+      elsif quantity == "overall"
+        s = I18n::translate('overall', :scope => 'line_items.quantity')
+      else
+        s = "#{times} #{I18n::translate(quantity, :scope => 'line_items.quantity')}"
+      end
+    else
+      s = "#{currency_fmt(times)} #{I18n::translate(quantity, :scope => 'line_items.quantity')}"
+    end
   end
 end
