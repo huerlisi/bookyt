c75416902ec96a80ad31167430978b23ddcbb6ad
Roman Simecek <roman.simecek@cyt.ch>
2011-11-07 16:05:47 +0100

    Role model improved.

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,9 +1,11 @@
 class Role < ActiveRecord::Base
   # Associations
   has_and_belongs_to_many :users
+  
+  validates_presence_of :name
 
   # Helpers
   def to_s
-    I18n.translate(name, :scope => 'cancan.roles')
+    name == nil ? "" : I18n.translate(name, :scope => 'cancan.roles')
   end
 end
