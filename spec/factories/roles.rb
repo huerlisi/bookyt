6c0f8eaa5a09ed56630a2dd32a9eb67952e850ad
Roman Simecek <roman.simecek@cyt.ch>
2011-11-07 14:15:15 +0100

    Role definition in fixtures changed.

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,5 +1,10 @@
 # Role factories
 FactoryGirl.define do
-  factory :role do
+  factory :admin_role, :class => Role do
+    name 'admin'
+  end
+  
+  factory :user_role, :class => Role do
+    name 'user'
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,14 +2,14 @@ FactoryGirl.define do
   factory :user do
     email    "user@example.com"
     password "user1234"
-    roles [Factory(:role, :name => 'user')]
     person
+    roles {|roles| [roles.association :user_role] }
   end
 
   factory :admin_user, :class => User do
     email    "admin@example.com"
     password "admin1234"
-    roles [Factory(:role, :name => 'admin')]
     person
+    roles {|roles| [roles.association :admin_role] }
   end
 end
