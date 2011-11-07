2bad1c61b7c5ca9f68b0e0ffbbaf5037058ac440
Roman Simecek <roman.simecek@cyt.ch>
2011-11-07 10:45:59 +0100

    Roles relations of the users fixed.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,23 +1,15 @@
 FactoryGirl.define do
-  factory :user_role, :class => Role do
-    name 'user'
-  end
-
-  factory :admin_role, :class => Role do
-    name 'admin'
-  end
-
   factory :user do
     email    "user@example.com"
     password "user1234"
-    association :user_role
+    roles [Factory(:role, :name => 'user')]
     person
   end
 
   factory :admin_user, :class => User do
     email    "admin@example.com"
     password "admin1234"
-    association :admin_role
+    roles [Factory(:role, :name => 'admin')]
     person
   end
 end
