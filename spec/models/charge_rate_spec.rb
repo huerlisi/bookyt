42a78d0e14e65902175904cf31b73648621ace07
Roman Simecek <roman.simecek@cyt.ch>
2011-11-10 15:28:56 +0100

    Tests for charge rate created.

--- spec/models/charge_rate_spec.rb
+++ spec/models/charge_rate_spec.rb
@@ -4,12 +4,20 @@ describe ChargeRate do
   it { should belong_to :person }
   
   context "when new" do
-    its(:to_s) { should == ": " }
+    @charge_rate = ChargeRate.new
+    
+    it "should return an empty string" do
+      @charge_rate.to_s.should =~ /: /
+      @charge_rate.to_s(:long).should == ":  ( - )"
+    end
   end
   
   context "when properly initialized" do
-    subject { Factory.build :charge_rate }
+    @charge_rate = Factory.build(:charge_rate)
 
-    its(:to_s) { should =~ /Title: 9.99/ }
+    it "should return as string the title" do
+      @charge_rate.to_s.should =~ /Title: 9.99/
+      @charge_rate.to_s(:long).should == "Title: 9.99 (14.04.2011 - 14.04.2011)"
+    end
   end
 end
