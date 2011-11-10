83d93346d32fc84f03f0f11ae3cb0d35eb0449f8
Roman Simecek <roman.simecek@cyt.ch>
2011-11-10 15:00:42 +0100

    Accountant role to factories added.

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -3,7 +3,11 @@ FactoryGirl.define do
   factory :admin_role, :class => Role do
     name 'admin'
   end
-  
+
+  factory :accountant_role, :class => Role do
+    name 'accountant'
+  end
+
   factory :user_role, :class => Role do
     name 'accountant'
   end
