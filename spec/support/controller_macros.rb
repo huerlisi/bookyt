f7b3e963522a7399e667b49f8d944cebb127d3a1
Roman Simecek <roman.simecek@cyt.ch>
2011-11-10 14:58:18 +0100

    Spec controller macro extended with accountant login and a logout afterwards.

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -4,5 +4,24 @@ module ControllerMacros
       @admin = Factory.create(:admin_user)
       sign_in(@admin)
     end
+    
+    after do
+      sign_out(@admin)
+      @admin.delete
+    end
+  end
+
+  def login_accountant
+    before do
+      @accountant = Factory.create(:accountant_user)
+      Ability.stub(:user).and_return(@accountant)
+      
+      sign_in(@accountant)
+    end
+    
+    after do
+      sign_out(@accountant)
+      @accountant.delete
+    end
   end
 end
