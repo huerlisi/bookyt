a38dfd0a8ab65f460fd7bc36efa338d9cc676994
Roman Simecek <roman.simecek@cyt.ch>
2011-11-10 16:01:38 +0100

    Spec for ChargeBookingTemplate created.

---
+++ spec/models/charge_booking_template_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe ChargeBookingTemplate do
+  context "when new" do
+    @template = ChargeBookingTemplate.new
+    
+    @template.charge_rate.should == nil
+    @template.amount.should == 0.0
+    @template.booking_parameters.should == {"comments" => nil, 
+                                            "debit_account_id" => nil,
+                                            "title" => nil,
+                                            "amount" => nil,
+                                            "credit_account_id" => nil}
+  end
+end
