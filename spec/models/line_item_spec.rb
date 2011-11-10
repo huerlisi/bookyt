4fdb3c090a323342586d4e0aca573f42460980db
Roman Simecek <roman.simecek@cyt.ch>
2011-11-10 16:03:48 +0100

    The should validates removed cause they dont work and amount tested.

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -3,10 +3,12 @@ require 'spec_helper'
 describe LineItem do
   it { should belong_to :item }
   it { should belong_to :invoice }
-  
-  pending "doesn't work at the moment" do
-    it { should validate_presence_of(:times) }
-    it { should validate_presence_of(:price) }
-    it { should validate_presence_of(:title) }
+
+  context "when new" do
+    its(:total_amount) { should == 0 }
+
+    pending "translations are missing" do
+      its(:times_to_s) { should == "" }
+    end
   end
 end
