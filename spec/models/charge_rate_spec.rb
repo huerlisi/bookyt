87e3976634c32e882bf3fc44803f962d42c8ce97
Roman Simecek <roman.simecek@cyt.ch>
2011-11-10 16:00:59 +0100

    To_s for charge_rate test coverd.

--- spec/models/charge_rate_spec.rb
+++ spec/models/charge_rate_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 
 describe ChargeRate do
   it { should belong_to :person }
-  
+
   context "when new" do
-    @charge_rate = ChargeRate.new
-    
     it "should return an empty string" do
-      @charge_rate.to_s.should =~ /: /
-      @charge_rate.to_s(:long).should == ":  ( - )"
+      @charge_rate = ChargeRate.new
+
+      @charge_rate.to_s.should == ""
+      @charge_rate.to_s(:long).should == ""
     end
   end
   
   context "when properly initialized" do
-    @charge_rate = Factory.build(:charge_rate)
-
     it "should return as string the title" do
-      @charge_rate.to_s.should =~ /Title: 9.99/
+      @charge_rate = Factory.build(:charge_rate)
+
+      @charge_rate.to_s.should == "Title: 9.99"
       @charge_rate.to_s(:long).should == "Title: 9.99 (14.04.2011 - 14.04.2011)"
     end
   end
