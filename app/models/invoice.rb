1a340c109f5f9e9d673c0be1f315b7f5e0613956
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-10 14:32:11 +0100

    Drop unused, duplicated Invoice.overdue scope.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,7 +4,6 @@ class Invoice < ActiveRecord::Base
 
   # Scopes
   default_scope order(:due_date)
-  scope :overdue, where("due_date < NOW()")
 
   # Associations
   belongs_to :customer, :class_name => 'Person'
