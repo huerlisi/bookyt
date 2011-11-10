e1bdf460c7f75823f2c7c054a534ff408a4dc3ac
Roman Simecek <roman.simecek@cyt.ch>
2011-11-10 13:49:49 +0100

    Spec for person written to increase the code coverage to 100%.

---
+++ spec/models/person_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Person do
+  its(:to_s) { should eq("") }
+
+  context "when new" do
+    @person = Person.new
+
+    @person.to_s.should ==""
+    @person.to_s(:long).should ==""
+  end
+
+  context "when existing" do
+    @person = Factory.create(:person)
+
+    @person.to_s.should =="Muster Peter"
+    @person.to_s(:default).should =="Muster Peter"
+    @person.to_s(:long).should =="Muster Peter (Capital)"
+  end
+end
