e6c15fbf2a6d021777e88ca1b39c4ae283c5b01f
Roman Simecek <roman.simecek@cyt.ch>
2011-11-11 07:55:25 +0100

    Rewritten the spec with shared_example for less duplicated code.

--- spec/controllers/tenants_controller_spec.rb
+++ spec/controllers/tenants_controller_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper'
 
 describe TenantsController do
-  describe "as admin" do
-    login_admin
-    
+  shared_examples "it does all controller actions" do
     describe "GET current" do
       it "get the current tenant" do
         get :current
@@ -20,22 +18,13 @@ describe TenantsController do
     end
   end
 
+  describe "as admin" do
+    login_admin
+    it_behaves_like "it does all controller actions"
+  end
+
   describe "as accountant" do
     login_accountant
-    
-    describe "GET current" do
-      it "get the current tenant" do
-        get :current
-        assigns(:tenant).should eq(subject.current_user.tenant)
-      end
-
-      pending "get the profit sheet" do
-        get :profit_sheet
-      end
-
-      pending "get blance sheet" do
-        get :balance_sheet
-      end
-    end
+    it_behaves_like "it does all controller actions"
   end
 end
