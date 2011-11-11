ecfe7db3e2c39f2b4bc3c72b456db6e01858c81c
Roman Simecek <roman.simecek@cyt.ch>
2011-11-11 07:36:43 +0100

    Missing controller actions added as pending.

--- spec/controllers/tenants_controller_spec.rb
+++ spec/controllers/tenants_controller_spec.rb
@@ -9,8 +9,17 @@ describe TenantsController do
         get :current
         assigns(:tenant).should eq(subject.current_user.tenant)
       end
+
+      pending "get the profit sheet" do
+        get :profit_sheet
+      end
+
+      pending "get blance sheet" do
+        get :balance_sheet
+      end
     end
   end
+
   describe "as accountant" do
     login_accountant
     
@@ -18,6 +27,14 @@ describe TenantsController do
       it "get the current tenant" do
         get :current
         assigns(:tenant).should eq(subject.current_user.tenant)
+      end
+
+      pending "get the profit sheet" do
+        get :profit_sheet
+      end
+
+      pending "get blance sheet" do
+        get :balance_sheet
       end
     end
   end
