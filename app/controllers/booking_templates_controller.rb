0dfee87c1661a9bc38710a8f13854579eb16a40b
Roman Simecek <roman.simecek@cyt.ch>
2011-11-11 15:20:57 +0100

    Redirect after update and create to index view.

--- app/controllers/booking_templates_controller.rb
+++ app/controllers/booking_templates_controller.rb
@@ -5,4 +5,16 @@ class BookingTemplatesController < AuthorizedController
     @booking_templates = BookingTemplate.paginate :page => params[:page], :per_page => params[:per_page], :order => order_by
     index!
   end
+
+  def create
+    create! do |success, failure|
+      success.html { redirect_to collection_path }
+    end
+  end
+
+  def update
+    update! do |success, failure|
+      success.html { redirect_to collection_path }
+    end
+  end
 end
