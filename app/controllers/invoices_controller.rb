5f4b8984898ac718b0ee7043bd392a98b84cbcb7
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-14 20:55:32 +0100

    Fix syntax in invoices#copy.

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -46,7 +46,7 @@ class InvoicesController < AuthorizedController
     @invoice.attributes = {
       :state         => 'booked',
       :value_date    => Date.today,
-      :due_date      => Date.today.in(30.days).to_date
+      :due_date      => Date.today.in(30.days).to_date,
       :duration_from => nil,
       :duration_to   => nil
     }
