c266f776544e1b337132c87db75655570fb62806
Roman Simecek <roman.simecek@cyt.ch>
2011-11-14 22:42:35 +0100

    TenantsController spec tried to improve.

--- spec/controllers/tenants_controller_spec.rb
+++ spec/controllers/tenants_controller_spec.rb
@@ -1,23 +1,32 @@
 require 'spec_helper'
 
-describe TenantsController do
-  shared_examples "it does all controller actions" do
-    describe "GET current" do
-      pending "get the current tenant" do
-        get :current
-        assigns(:tenant).should eq(subject.current_user.tenant)
-      end
+shared_examples "it does all controller actions" do
+  context "get the current tenant" do
+    it "should have a current_user" do
+      subject.current_user.should_not be_nil
+    end
 
-      pending "get the profit sheet" do
-        get :profit_sheet
-      end
-
-      pending "get blance sheet" do
-        get :balance_sheet
-      end
+    it "redirects to the current tenant" do
+      controller.stub(:current_user).and_return(subject.current_user)
+      get :current
+      response.should redirect_to(current_user.tenant)
     end
   end
 
+  context "get the profit sheet" do
+    it "show with the current data" do
+      get :profit_sheet
+    end
+  end
+
+  context "get blance sheet" do
+    it "show with the current data" do
+      get :balance_sheet
+    end
+  end
+end
+
+describe TenantsController do
   describe "as admin" do
     login_admin
     it_behaves_like "it does all controller actions"
