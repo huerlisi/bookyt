72134b6a118dbd16987e4570f4d4449aeb52d670
Roman Simecek <roman.simecek@cyt.ch>
2011-11-16 07:40:06 +0100

    Rspec test for booking_helper fixed.

--- spec/helpers/booking_helper_spec.rb
+++ spec/helpers/booking_helper_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe BookingHelper do
   describe "#reference_types_as_collection" do
-    context "returns the reference types" do
+    it "returns the reference types" do
+      helper.stub(:t_model).and_return('Rechnung')
       subject { helper.reference_types_as_collection }
-      its(["Rechnung"]) { should eq(Invoice.base_class)}
+      subject { should eq(Invoice.base_class)}
     end
   end
 end
