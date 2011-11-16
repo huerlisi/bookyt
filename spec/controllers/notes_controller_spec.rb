183b0e9a05d71a2b7de68a233c0b9772081e344e
Roman Simecek <roman.simecek@cyt.ch>
2011-11-16 16:37:53 +0100

    NotesController spec finished for the methods new and create.

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -3,23 +3,32 @@ require 'spec_helper'
 shared_examples "note actions" do
   describe "interact with notes controller" do
     it "should list the notes" do
-      employee = Factory.create(:employee)
-      get :index, :employee_id => employee.id
+      get :index, :employee_id => @employee.id
       assigns(:note_of_sth).should_not be_nil
       assigns(:notes).should_not be_nil
+      assigns(:notes).should_not be_empty
     end
 
-    pending "should display a new note" do
-      get :new
+    it "should display a new note" do
+      get :new, :employee_id => @employee.id
+      response.should render_template('new')
+      assigns(:note).should_not be_nil
+      assigns(:note).should be_a_kind_of(Note)
+      assigns(:note).user.should eq(@current_user)
     end
     
-    pending "should create a note" do
-      get :create
+    it "should create a note" do
+      post :create, {:employee_id => @employee.id, :note => {:content => 'Comment'}}
+      response.should redirect_to(employee_notes_path(@employee))
     end
   end
 end
 
 describe NotesController do
+  before(:all) do
+    @employee = Factory.create(:employee)
+  end
+
   context "as admin" do
     login_admin
     it_behaves_like "note actions"
