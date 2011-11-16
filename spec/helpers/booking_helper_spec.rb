03c6342366f93dbc730224b3b5cb187fd7c23af3
Roman Simecek <roman.simecek@cyt.ch>
2011-11-16 09:34:46 +0100

    Helper spec improved.

--- spec/helpers/booking_helper_spec.rb
+++ spec/helpers/booking_helper_spec.rb
@@ -4,8 +4,7 @@ describe BookingHelper do
   describe "#reference_types_as_collection" do
     it "returns the reference types" do
       helper.stub(:t_model).and_return('Rechnung')
-      subject { helper.reference_types_as_collection }
-      subject { should eq(Invoice.base_class)}
+      helper.reference_types_as_collection.should eq({'Rechnung' => Invoice.base_class})
     end
   end
 end
