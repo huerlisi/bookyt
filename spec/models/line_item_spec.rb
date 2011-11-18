13ac90146635f89b5162e5cb53331d4a08c97000
Roman Simecek <roman.simecek@cyt.ch>
2011-11-18 08:43:25 +0100

    LineItem spec finished.

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -3,12 +3,37 @@ require 'spec_helper'
 describe LineItem do
   it { should belong_to :item }
   it { should belong_to :invoice }
-  # it { should belong_to :booking_template }
-
+  it { should belong_to :contra_account }
   context "when new" do
-    subject { LineItem.new }
-    its(:total_amount) { should eq(0) }
-    its(:times_to_s) { should eq(" x") }
-    its(:vat_rate) { should eq(0) }
+    describe "blank attributes" do
+      subject { LineItem.new }
+      its(:total_amount) { should eq(0) }
+      its(:times_to_s) { should eq(" x") }
+      its(:vat_rate) { should eq(0) }
+    end
+    describe "with x quantity" do
+      subject { LineItem.new(:quantity => 'x', :times => 1, :price => 12.25) }
+      its(:times) { should eq(1) }
+      its(:times_to_s) { should eq('') }
+      its(:vat_rate) { should eq(0) }
+      its(:total_amount) { should eq(12.25) }
+      its(:price) { should eq(12.25) }
+    end
+    describe "with overall quantity" do
+      subject { LineItem.new(:quantity => 'overall', :times => 1, :price => 12.25) }
+      its(:times) { should eq(1) }
+      its(:times_to_s) { should eq(I18n::translate('line_items.quantity.overall')) }
+      its(:vat_rate) { should eq(0) }
+      its(:total_amount) { should eq(12.25) }
+      its(:price) { should eq(12.25) }
+    end
+    describe "without quantity" do
+      subject { LineItem.new(:quantity => 'hours', :times => 1.0, :price => 12.25) }
+      its(:times) { should eq(1) }
+      its(:times_to_s) { should eq("1.0 #{I18n::translate('line_items.quantity.hours')}") }
+      its(:vat_rate) { should eq(0) }
+      its(:total_amount) { should eq(12.25) }
+      its(:price) { should eq(12.25) }
+    end
   end
 end
