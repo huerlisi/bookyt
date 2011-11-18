81f5c42e5b25fd6701f5d3fca06f79fb87b16c96
Roman Simecek <roman.simecek@cyt.ch>
2011-11-18 08:42:52 +0100

    Attachment factory extended with the object association.

--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :attachment do
     title "MyString"
     file  { File.open(File.join(Rails.root, 'spec', 'lib', 'templates', 'letter.pdf')) }
+    association :object, :factory => :employee
   end
 end
