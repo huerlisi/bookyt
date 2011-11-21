56392554624b276e5951241998115f9acb83ac96
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-21 10:26:41 +0100

    Restructure Employee class.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,9 +1,11 @@
 class Employee < Person
   # Associations
-  has_many :employments
-  has_many :employers, :through => :employments
   has_many :salaries, :foreign_key => :company_id
 
   # Charge Rates
   has_many :charge_rates, :foreign_key => :person_id
+
+  # Employments
+  has_many :employments
+  has_many :employers, :through => :employments
 end
