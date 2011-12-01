fc7ce53f247aab54e6c38141b51cf3bb5adb767f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-01 09:58:04 +0100

    Don't configure production mode to use X-Sendfile. This can be set in the web-server config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Bookyt::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile"
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
