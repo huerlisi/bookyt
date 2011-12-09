7cd246c28f437734080502d36e1dd05ef7194296
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-09 13:30:49 +0100

    Only show open invoices in people view by default.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -12,7 +12,7 @@ class PeopleController < AuthorizedController
     @debit_invoices = resource.debit_invoices
 
     # Invoice scoping by state
-    by_state = params[:by_state] || 'all'
+    by_state = params[:by_state] || 'booked'
     @credit_invoices = @credit_invoices.reorder('due_date DESC').by_state(by_state)
     @debit_invoices = @debit_invoices.reorder('due_date DESC').by_state(by_state)
 
