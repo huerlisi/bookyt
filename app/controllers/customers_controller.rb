581045491dc39a0dc78d9b0179be360b0d140263
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-09 14:22:35 +0100

    Unify people views.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,12 +1,3 @@
 class CustomersController < PeopleController
   defaults :resource_class => Customer
-
-  def show
-    # Invoice scoping by state
-    by_state = params[:by_state] || 'all'
-    @open_debit_invoices = resource.invoices.reorder('due_date DESC').by_state('booked')
-    @paid_debit_invoices = resource.invoices.reorder('due_date DESC').by_state(['canceled', 'paid'])
-
-    show!
-  end
 end
