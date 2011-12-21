7831e4d3e11b662ab53df2805b6eb2b8d7ff8487
Roman Simecek <roman.simecek@cyt.ch>
2011-12-21 09:24:32 +0100

    Create a new spec for the people controller.

---
+++ spec/controllers/people_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+shared_examples "people actions" do
+  describe "show" do
+    it "the person" do
+      get :show, :id => 200000
+      response.should render_template('show')
+      assigns(:person).should_not be_nil
+      assigns(:person).should be_an_instance_of(Person)
+    end
+
+    it "the debit invoices" do
+      get :show, :id => 200000
+      response.should render_template('show')
+      assigns(:debit_invoices).should be_empty
+=begin
+      assigns(:debit_invoices).count.should be(3)
+      assigns(:debit_invoices).first.should be_an_instance_of(DebitInvoice)
+=end
+    end
+
+    it "the credit invoices" do
+      get :show, :id => 200000
+      response.should render_template('show')
+      assigns(:credit_invoices).should be_empty
+=begin
+      assigns(:credit_invoices).count.should be(3)
+      assigns(:credit_invoices).first.should be_an_instance_of(DebitInvoice)
+=end
+    end
+  end
+end
+
+describe PeopleController do
+  before(:all) do
+    Factory.create(:person, :id => 200000)
+  end
+
+  context "as admin" do
+    login_admin
+    it_behaves_like "people actions"
+  end
+
+  context "as accountant" do
+    login_accountant
+    it_behaves_like "people actions"
+  end  
+end
