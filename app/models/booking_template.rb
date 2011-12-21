dd9a3e73135dccbcc49ead3164364a0ecc45e134
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-21 02:15:27 +0100

    Use method instead of attribute to get amount for booking template.

--- app/models/booking_template.rb
+++ app/models/booking_template.rb
@@ -33,7 +33,7 @@ class BookingTemplate < ActiveRecord::Base
     booking_params = attributes.reject{|key, value| !["title", "comments", "credit_account_id", "debit_account_id"].include?(key)}
 
     # Calculate amount
-    booking_amount = BigDecimal.new(attributes['amount'] || '0')
+    booking_amount = BigDecimal.new(self.amount.to_s || '0')
 
     # Lookup reference
     reference = params['reference']
