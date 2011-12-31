f6366e6720cfd55dc143b8d5f6a60bcba41b3558
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-31 10:57:51 +0100

    Fix indentation and EOL.

--- spec/factories/line_items.rb
+++ spec/factories/line_items.rb
@@ -1,14 +1,12 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :line_item do
-      quantity "9.99"
-      price "9.99"
-      code "MyString"
-      title "MyString"
-      description "MyString"
-      item nil
-      type ""
-      invoice nil
-    end
+    quantity "9.99"
+    price "9.99"
+    code "MyString"
+    title "MyString"
+    description "MyString"
+    item nil
+    type ""
+    invoice nil
+  end
 end
