14b01180b7ba6785f2402513afb9a3fdf7e24c4e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-05 22:16:03 +0100

    More LineItem factories.

--- spec/factories/line_items.rb
+++ spec/factories/line_items.rb
@@ -1,9 +1,26 @@
 FactoryGirl.define do
-  factory :line_item do
-    quantity "9.99"
-    price "9.99"
-    code "MyString"
-    title "MyString"
-    description "MyString"
+  factory :banana do
+    times "1"
+    quantity "x"
+    price "0.99"
+    title "Banana"
+    description "Most delicious banana of all time!"
+  end
+
+  factory :vat do
+    times "1"
+    quantity "%"
+    price "8"
+    code "vat:full"
+    title "VAT"
+    description "VAT"
+  end
+
+  factory :contracting do
+    times "5"
+    quantity "hours"
+    price "180"
+    title "Contracting"
+    description "Custom development by CyT! Best price for best product"
   end
 end
