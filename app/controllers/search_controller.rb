52493a8f6f5f818f751e30cf0a5789db9d2bf66e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-08 11:26:55 +0100

    Allow partial matches in global search.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def search
     # TODO: get "undefined method `sphinx_index_options'" when Person and any other class is given???
-    @people = ThinkingSphinx.search(params[:query], :classes => [Person])
-    @invoices = ThinkingSphinx.search(params[:query], :classes => [Invoice])
+    @people = ThinkingSphinx.search(params[:query], :classes => [Person], :star => true)
+    @invoices = ThinkingSphinx.search(params[:query], :classes => [Invoice], :star => true)
   end
 end
