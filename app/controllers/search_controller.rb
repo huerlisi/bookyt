e1924d8b65f762190ba95d17be5b26b8ba56379e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-08 11:18:26 +0100

    Use tabs for search result grouping.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def search
     # TODO: get "undefined method `sphinx_index_options'" when Person and any other class is given???
-    @records = ThinkingSphinx.search(params[:query], :classes => [Invoice, Company, Customer, Employee, Bank])
+    @people = ThinkingSphinx.search(params[:query], :classes => [Person])
+    @invoices = ThinkingSphinx.search(params[:query], :classes => [Invoice])
   end
 end
