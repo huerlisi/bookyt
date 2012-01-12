beae098afc5d7f77b7604b1b22b8aef9b2d20353
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-12 13:10:45 +0100

    Better line item factories.

--- spec/factories/line_items.rb
+++ spec/factories/line_items.rb
@@ -1,26 +1,28 @@
 FactoryGirl.define do
-  factory :banana do
-    times "1"
-    quantity "x"
-    price "0.99"
-    title "Banana"
-    description "Most delicious banana of all time!"
-  end
+  factory :line_item do
+    factory :banana do
+      times "1"
+      quantity "x"
+      price "0.99"
+      title "Banana"
+      description "Most delicious banana of all time!"
+    end
 
-  factory :vat do
-    times "1"
-    quantity "%"
-    price "8"
-    code "vat:full"
-    title "VAT"
-    description "VAT"
-  end
+    factory :vat do
+      times "1"
+      quantity "%"
+      price "8"
+      code "vat:full"
+      title "VAT"
+      description "VAT"
+    end
 
-  factory :contracting do
-    times "5"
-    quantity "hours"
-    price "180"
-    title "Contracting"
-    description "Custom development by CyT! Best price for best product"
+    factory :contracting do
+      times "5"
+      quantity "hours"
+      price "180"
+      title "Contracting"
+      description "Custom development by CyT! Best price for best product"
+    end
   end
 end
