11d7840c0ba64b7815e37db41ef8558a383e7453
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-18 23:53:47 +0100

    Some locale setting fixes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
   # Authentication
   before_filter :authenticate_user!
 
+  # Set the user locale
+  before_filter :set_locale
+  def set_locale
+    I18n.locale = current_user.locale if current_user
+  end
+
   # Tenancy
   def current_tenant
     current_user.tenant
