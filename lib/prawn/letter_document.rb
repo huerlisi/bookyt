9b30b771d4b0dc6f2a98765463fcefe22934fdd1
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-18 22:42:14 +0100

    Move payslip specific prawn methods from bookyt to bookyt_salary.

--- lib/prawn/letter_document.rb
+++ lib/prawn/letter_document.rb
@@ -142,19 +142,6 @@ module Prawn
       items_table(rows)
     end
 
-    def salary_table(direct_balance, direct_account, direct_bookings)
-      content = direct_bookings.inject([]) do |out, item|
-        title = item.title if item
-        out << [title, nil, nil, nil, currency_fmt(item.accounted_amount(direct_account))] if item.contra_account(direct_account)
-
-        out
-      end
-
-      rows = content + [total_row(currency_fmt(direct_balance))]
-
-      items_table(rows)
-    end
-
     def total_row(amount)
       ["Total", nil, nil, nil, amount]
     end
