8a4df5d3a49de7ce6f541c4fc241c24f5e46da0b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-02-23 10:12:29 +0100

    Port raiffeisen quick_book.sh to use Account with code 1080 for all imported bookings.

--- data/quick-book.sh
+++ data/quick-book.sh
@@ -54,26 +54,26 @@ function csv2booking() {
 local credit_account="$1"
 local debit_account="$2"
 
-  awk -F ';' '{print "Booking.create(:title => \""$4"\", :comments => \""$2"\", :debit_account_id => '$debit_account', :credit_account_id => '$credit_account', :amount => (" $3 ").abs, :value_date => \"" $5 "\")"}'
+  awk -F ';' '{print "Booking.create(:title => \""$4"\", :comments => \""$2"\", :debit_account_id => Account.find_by_code(\""'$debit_account'"\").id, :credit_account_id => Account.find_by_code(\""'$credit_account'"\").id, :amount => (" $3 ").abs, :value_date => \""$5"\")"}'
 }
 
 
-cat "$file" | fix_csv | add_value_date | parse_chf_ebanking 'E-Banking' | grep -v 'Details unterdrückt' | csv2booking 40 2
-cat "$file" | fix_csv | add_value_date | parse_chf_ebanking 'Lohn' | grep 'Details unterdrückt' | csv2booking 32 2
-cat "$file" | fix_csv | add_value_date | parse_chf_ebanking 'E-Banking Auftrag' | grep 'Details unterdrückt' | csv2booking 32 2
-cat "$file" | fix_csv | add_value_date | add_title "Sollzins" | csv2booking 39 2
-cat "$file" | fix_csv | add_value_date | add_title "Kreditkommission" | csv2booking 39 2
-cat "$file" | fix_csv | add_value_date | add_title "Habenzins" | csv2booking 2 38
-cat "$file" | fix_csv | add_value_date | add_title "Verrechnungssteuer" | csv2booking 50 2
-cat "$file" | fix_csv | add_value_date | add_title "Kontoführungsgebühren" | csv2booking 39 2
-cat "$file" | fix_csv | add_value_date | add_title "Gebühren" | csv2booking 39 2
-cat "$file" | fix_csv | add_value_date | add_title "gebühr" "Gebühren"| csv2booking 39 2
-cat "$file" | fix_csv | add_value_date | add_title "Versandspesen" | csv2booking 39 2
-cat "$file" | fix_csv | add_value_date | add_chf_value_eur_ebanking | csv2booking 40 2
-cat "$file" | fix_csv | add_value_date | comment_as_title 'Barauszahlung' | csv2booking 1 2
-cat "$file" | fix_csv | add_value_date | add_title '[Bb]ancomat[-]?[Bb]ezug' 'Bancomatbezug' | csv2booking 1 2
-cat "$file" | fix_csv | add_value_date | add_title 'Bareinzahlung' | grep -v 'Gebühr' | csv2booking 2 1
-cat "$file" | fix_csv | add_value_date | add_title 'Einzahlung' | csv2booking 2 1
-cat "$file" | fix_csv | add_value_date | add_value_and_title | grep 'Aduno' | csv2booking 2 49
-cat "$file" | fix_csv | add_value_date | add_value_and_title | grep 'Gutschrift' | grep -v 'Gutschrift VESR' | grep -v 'Aduno' | csv2booking 2 52
-cat "$file" | fix_csv | add_value_date | add_value_and_title | grep 'Postvergütung' | csv2booking 2 52
+cat "$file" | fix_csv | add_value_date | parse_chf_ebanking 'E-Banking' | grep -v 'Details unterdrückt' | csv2booking 1080 1020
+cat "$file" | fix_csv | add_value_date | parse_chf_ebanking 'Lohn' | grep 'Details unterdrückt' | csv2booking 1080 1020
+cat "$file" | fix_csv | add_value_date | parse_chf_ebanking 'E-Banking Auftrag' | grep 'Details unterdrückt' | csv2booking 1080 1020
+cat "$file" | fix_csv | add_value_date | add_title "Sollzins" | csv2booking 1080 1020
+cat "$file" | fix_csv | add_value_date | add_title "Kreditkommission" | csv2booking 1080 1020
+cat "$file" | fix_csv | add_value_date | add_title "Habenzins" | csv2booking 1020 1080
+cat "$file" | fix_csv | add_value_date | add_title "Verrechnungssteuer" | csv2booking 1080 1020
+cat "$file" | fix_csv | add_value_date | add_title "Kontoführungsgebühren" | csv2booking 1080 1020
+cat "$file" | fix_csv | add_value_date | add_title "Gebühren" | csv2booking 1080 1020
+cat "$file" | fix_csv | add_value_date | add_title "gebühr" "Gebühren"| csv2booking 1080 1020
+cat "$file" | fix_csv | add_value_date | add_title "Versandspesen" | csv2booking 1080 1020
+cat "$file" | fix_csv | add_value_date | add_chf_value_eur_ebanking | csv2booking 1080 1020
+cat "$file" | fix_csv | add_value_date | comment_as_title 'Barauszahlung' | csv2booking 1080 1020
+cat "$file" | fix_csv | add_value_date | add_title '[Bb]ancomat[-]?[Bb]ezug' 'Bancomatbezug' | csv2booking 1080 1020
+cat "$file" | fix_csv | add_value_date | add_title 'Bareinzahlung' | grep -v 'Gebühr' | csv2booking 1020 1080
+cat "$file" | fix_csv | add_value_date | add_title 'Einzahlung' | csv2booking 1020 1080
+cat "$file" | fix_csv | add_value_date | add_value_and_title | grep 'Aduno' | csv2booking 1020 1080
+cat "$file" | fix_csv | add_value_date | add_value_and_title | grep 'Gutschrift' | grep -v 'Gutschrift VESR' | grep -v 'Aduno' | csv2booking 1020 1080
+cat "$file" | fix_csv | add_value_date | add_value_and_title | grep 'Postvergütung' | csv2booking 1020 1080
