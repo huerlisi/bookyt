f80b78f226b99559cf80978449a23fb44f0173b5
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-03-19 11:27:29 +0100

    Fix LineItem building migration for Invoices.

--- db/migrate/20111118152144_add_line_items_to_debit_invoices.rb
+++ db/migrate/20111118152144_add_line_items_to_debit_invoices.rb
@@ -3,7 +3,7 @@ class AddLineItemsToDebitInvoices < ActiveRecord::Migration
     invoices = DebitInvoice.all.select{|i| i.line_items.empty? }
 
     for invoice in invoices
-      line_item = invoice.line_items.build(:title => invoice.title, :times => 1, :quantity => 'x', :price => invoice.amount, :contra_account => invoice.profit_account)
+      line_item = invoice.line_items.build(:title => invoice.title, :times => 1, :quantity => 'x', :price => invoice.amount, :contra_account_id => invoice.profit_account.id)
       line_item.save
     end
   end
