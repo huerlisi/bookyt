251ea72a43620b2b2021009c7a4922a8d7730dd1
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-03-19 11:46:14 +0100

    Fix UseDebitAndCreditAccountsForLineItems migration.

--- db/migrate/20111227100728_use_debit_and_credit_accounts_for_line_items.rb
+++ db/migrate/20111227100728_use_debit_and_credit_accounts_for_line_items.rb
@@ -6,18 +6,18 @@ class UseDebitAndCreditAccountsForLineItems < ActiveRecord::Migration
     add_column :line_items, :debit_account_id, :integer
     add_index :line_items, :debit_account_id
 
-    LineItem.find_each do |line_item|
-      invoice = line_item.invoice
-      case invoice.class.name
-      when "DebitInvoice"
-        line_item.credit_account_id = invoice.direct_account.id
-        line_item.debit_account_id  = line_item.contra_account_id
-      when "CreditInvoice"
-        line_item.credit_account_id = line_item.contra_account_id
-        line_item.debit_account_id  = invoice.direct_account.id
+    LineItem.unscoped do
+      LineItem.find_each do |line_item|
+        invoice = line_item.invoice
+        case invoice.class.name
+        when "DebitInvoice"
+          line_item.update_column(:credit_account_id, invoice.direct_account.id)
+          line_item.update_column(:debit_account_id, line_item.contra_account_id)
+        when "CreditInvoice"
+          line_item.update_column(:credit_account_id, line_item.contra_account_id)
+          line_item.update_column(:debit_account_id, invoice.direct_account.id)
+        end
       end
-
-      line_item.save(:validate => false)
     end
 
     remove_column :line_items, :contra_account_id
