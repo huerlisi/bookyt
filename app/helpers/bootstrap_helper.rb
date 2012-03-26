a1e89f49f47aa68171a01ff48e0f0e2149523fba
Andi Staub (CyT) <andi.staub@cyt.ch>
2012-03-26 14:15:18 +0200

    Labels updated to bootstrap2.

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -10,7 +10,7 @@ module BootstrapHelper
   def boot_label(content, type = nil)
     return "" unless content.present?
 
-    classes = ['label', type].compact.join(' ')
+    classes = ['label', "label-#{type}"].compact.join(' ')
     content_tag(:span, content, :class => classes)
   end
 
