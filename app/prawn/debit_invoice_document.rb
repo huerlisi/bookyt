d4bfdae8d1d377ae4804f8a24cc0f7ea25a33371
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-04-10 10:36:12 +0200

    Use more compact wording in closing for debit invoices.

--- app/prawn/debit_invoice_document.rb
+++ app/prawn/debit_invoice_document.rb
@@ -1,3 +1,13 @@
 class DebitInvoiceDocument < LetterDocument
   include Prawn::EsrRecipe
+
+  def invoice_closing(sender, due_date)
+    text " "
+    text I18n.t('letters.debit_invoice.closing', :due_date => due_date), :align => :justify
+
+    text " "
+
+    text I18n.t('letters.debit_invoice.greetings')
+    text "#{sender.vcard.full_name}"
+  end
 end
