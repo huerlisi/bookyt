7757747965896df121b2680985944de142980498
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-05-02 10:37:20 +0200

    Support full, reduced, and special vat rates in expenses form.

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,8 +4,21 @@ class ExpensesController < ApplicationController
       :debit_account => Account.find_by_code('1000')
     )
 
-    @vat_booking = Booking.new(
-      :title => "MwSt. 8%",
+    @vat_full_rate = ChargeRate.current('vat:full', Date.today)
+    @vat_full_booking = Booking.new(
+      :title => @vat_full_rate.title,
+      :debit_account => Account.find_by_code('1000'),
+      :credit_account => Account.find_by_code('1170')
+    )
+    @vat_reduced_rate = ChargeRate.current('vat:reduced', Date.today)
+    @vat_reduced_booking = Booking.new(
+      :title => @vat_reduced_rate.title,
+      :debit_account => Account.find_by_code('1000'),
+      :credit_account => Account.find_by_code('1170')
+    )
+    @vat_special_rate = ChargeRate.current('vat:special', Date.today)
+    @vat_special_booking = Booking.new(
+      :title => @vat_special_rate.title,
       :debit_account => Account.find_by_code('1000'),
       :credit_account => Account.find_by_code('1170')
     )
@@ -13,17 +26,22 @@ class ExpensesController < ApplicationController
 
   def create
     @booking = Booking.new(params[:booking])
-    @vat_booking = Booking.new(params[:vat_booking])
+    @vat_full_booking = Booking.new(params[:vat_full_booking])
+    @vat_reduced_booking = Booking.new(params[:vat_reduced_booking])
+    @vat_special_booking = Booking.new(params[:vat_special_booking])
 
     # Use same debit_account for vat bookings
-    @vat_booking.value_date = @booking.value_date
-    @vat_booking.debit_account = @booking.debit_account
+    @vat_full_booking.value_date = @booking.value_date
+    @vat_full_booking.debit_account = @booking.debit_account
+    @vat_reduced_booking.value_date = @booking.value_date
+    @vat_reduced_booking.debit_account = @booking.debit_account
+    @vat_special_booking.value_date = @booking.value_date
+    @vat_special_booking.debit_account = @booking.debit_account
 
-    if @booking.valid? && @vat_booking.valid?
-      @booking.save && @vat_booking.save
+    if @booking.valid? && @vat_full_booking.valid? && @vat_reduced_booking.valid? && @vat_special_booking.valid?
+      @booking.save && @vat_full_booking.save && @vat_reduced_booking.save && @vat_special_booking.valid?
       redirect_to "/expenses/new"
     else
-      raise @vat_booking.errors.inspect
       render :action => 'new'
     end
   end
