a13f694b0c7950f9057d88fa13a0b034ec9fdd28
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-05-02 10:30:50 +0200

    Fix db seed for vat:special ChargeRate.

--- db/seeds/locales/de-CH.rb
+++ db/seeds/locales/de-CH.rb
@@ -6,7 +6,7 @@
 ChargeRate.create!([
   {:code => 'vat:full', :title => "MWSt. (Normaler Satz)", :duration_from => '2011-01-01', :rate => 8},
   {:code => 'vat:reduced', :title => "MWSt. (Reduzierter Satz)", :duration_from => '2011-01-01', :rate => 2.5},
-  {:code => 'vat:special', :title => "MWSt. (Sondersatz)", :duration_from => '2011-01-01', :rate => 2.5},
+  {:code => 'vat:special', :title => "MWSt. (Sondersatz)", :duration_from => '2011-01-01', :rate => 3.8},
   {:code => 'vat:excluded', :title => "MWSt. befreit", :duration_from => '2011-01-01', :rate => 0},
 ])
 
