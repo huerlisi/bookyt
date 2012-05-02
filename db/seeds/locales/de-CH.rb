3cb882f3936b7da2decb1086a3bbe4dbc5d48fa5
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-05-02 11:22:07 +0200

    VAT rates shall be relative and have shorter titles in db seed.

--- db/seeds/locales/de-CH.rb
+++ db/seeds/locales/de-CH.rb
@@ -4,10 +4,10 @@
 
 # Vat Rates
 ChargeRate.create!([
-  {:code => 'vat:full', :title => "MWSt. (Normaler Satz)", :duration_from => '2011-01-01', :rate => 8},
-  {:code => 'vat:reduced', :title => "MWSt. (Reduzierter Satz)", :duration_from => '2011-01-01', :rate => 2.5},
-  {:code => 'vat:special', :title => "MWSt. (Sondersatz)", :duration_from => '2011-01-01', :rate => 3.8},
-  {:code => 'vat:excluded', :title => "MWSt. befreit", :duration_from => '2011-01-01', :rate => 0},
+  {:code => 'vat:full', :title => "MWSt.", :duration_from => '2011-01-01', :rate => 8, :relative => true},
+  {:code => 'vat:reduced', :title => "MWSt.", :duration_from => '2011-01-01', :rate => 2.5, :relative => true},
+  {:code => 'vat:special', :title => "MWSt.", :duration_from => '2011-01-01', :rate => 3.8, :relative => true},
+  {:code => 'vat:excluded', :title => "MWSt. befreit", :duration_from => '2011-01-01', :rate => 0, :relative => true},
 ])
 
 CivilStatus.create!([
