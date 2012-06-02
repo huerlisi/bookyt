7554ab8effca4656ac54ab7896efc53f4725a020
Roman Simecek <roman.simecek@awd.ch>
2012-06-03 00:23:23 +0200

    Test updated.

--- spec/models/charge_booking_template_spec.rb
+++ spec/models/charge_booking_template_spec.rb
@@ -6,10 +6,13 @@ describe ChargeBookingTemplate do
 
     @template.charge_rate.should == nil
     @template.amount.should == 0.0
-    @template.booking_parameters.should == {"comments" => nil,
-                                            "debit_account_id" => nil,
-                                            "title" => nil,
-                                            "amount" => 0.0,
-                                            "credit_account_id" => nil}
+    @template.booking_parameters.should == {
+                                             "comments"          => nil,
+                                             "debit_account_id"  => nil,
+                                             "title"             => nil,
+                                             "amount"            => BigDecimal(0),
+                                             "credit_account_id" => nil,
+                                             "include_in_saldo"  =>[],
+                                            }
   end
 end
