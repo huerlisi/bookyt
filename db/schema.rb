5b935e86cd0fc0da2ce80458497f51292f6d341a
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-07-02 14:02:39 +0200

    Add db/schema.rb version bump.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120630212326) do
+ActiveRecord::Schema.define(:version => 20120702112651) do
 
   create_table "account_types", :force => true do |t|
     t.string   "name",       :limit => 100
