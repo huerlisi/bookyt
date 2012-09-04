226f2c8303af4ba8e7c933fe38d0f9125f3e11f5
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-09-04 10:24:51 +0200

    Mark direct_bookings_controller spec as pending as direct_bookings is currently not really used.

--- spec/controllers/direct_bookings_controller_spec.rb
+++ spec/controllers/direct_bookings_controller_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 shared_examples "direct booking actions" do
   describe "should create a new direct booking" do
-    it "creates a new direct booking" do
+    pending "creates a new direct booking" do
       booking_template = Factory.create(:invoice_booking_template)
       xhr :get, :new, {:direct_booking => {:booking_template_id => booking_template.id}}
       response.should render_template('new')
       assigns(:direct_booking).value_date.should eq(Date.today)
     end
 
-    it "creates a direct booking" do
+    pending "creates a direct booking" do
       invoice = Factory.create(:invoice)
       credit_account = Factory.create(:credit_account)
       debit_account = Factory.create(:service_account)
