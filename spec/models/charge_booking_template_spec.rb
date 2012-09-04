feffc5187fd885cf23813a8968e639a9bd573b8b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-09-04 09:51:56 +0200

    Move test to "it" block in charge_booking_template spec.

--- spec/models/charge_booking_template_spec.rb
+++ spec/models/charge_booking_template_spec.rb
@@ -2,16 +2,18 @@ require 'spec_helper'
 
 describe ChargeBookingTemplate do
   context "when new" do
-    @template = ChargeBookingTemplate.new
+    it "should build proper record" do
+      @template = ChargeBookingTemplate.new
 
-    @template.charge_rate.should == nil
-    @template.amount.should == 0.0
-    @template.booking_parameters.should == {
-                                             "comments"          => nil,
-                                             "debit_account_id"  => nil,
-                                             "title"             => nil,
-                                             "amount"            => BigDecimal("0"),
-                                             "credit_account_id" => nil
-                                            }
+      @template.charge_rate.should == nil
+      @template.amount.should == 0.0
+      @template.booking_parameters.should == {
+                                               "comments"          => nil,
+                                               "debit_account_id"  => nil,
+                                               "title"             => nil,
+                                               "amount"            => BigDecimal("0"),
+                                               "credit_account_id" => nil
+                                              }
+    end
   end
 end
