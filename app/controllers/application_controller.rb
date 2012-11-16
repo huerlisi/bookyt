668836aa7db83a0942634d93e1eed25b65bfb76a
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-11-16 20:19:50 +0100

    Fix default locale fallback.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   # Set the user locale
   before_filter :set_locale
   def set_locale
-    locale = params[:locale] || cookies[:locale]
+    locale = params[:locale].presence || cookies[:locale].presence || I18n.default_locale
     I18n.locale = locale.to_s
     cookies[:locale] = locale unless (cookies[:locale] && cookies[:locale] == locale.to_s)
   end
