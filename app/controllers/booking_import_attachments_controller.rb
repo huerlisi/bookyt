74205d60322a119375f60e1ae464b367cf7eede5
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-11-21 15:02:37 +0100

    Preset encoding to ISO-8859-15 for booking import attachments.

--- app/controllers/booking_import_attachments_controller.rb
+++ app/controllers/booking_import_attachments_controller.rb
@@ -1,6 +1,12 @@
 class BookingImportAttachmentsController < AttachmentsController
   defaults :resource_class => BookingImportAttachment
 
+  def new
+    resource.encoding = 'ISO-8859-15'
+
+    new!
+  end
+
   def create
     create! {
       new_booking_import_attachment_mt940_import_path(resource)
