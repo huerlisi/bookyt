822ffefbec48b8753101149d643d1fbfba81f519
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-04-23 09:42:08 +0200

    Add dependency checks to deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,3 +30,16 @@ set :use_sudo, false
 set :deploy_via, :remote_cache
 set :git_enable_submodules, 1
 set :copy_exclude, [".git", "spec"]
+
+# Dependencies
+depend :remote, :gem, 'bundler', '> 0'
+depend :remote, :gem, 'bluepill', ''
+
+# Headers for gem compilation
+depend :remote, :deb, "build-essential", ''
+depend :remote, :deb, "ruby1.9.1-dev", ''
+depend :remote, :deb, "libmysqlclient-dev", ''
+depend :remote, :deb, "libsqlite3-dev", ''
+depend :remote, :deb, "libxml2-dev", ''
+depend :remote, :deb, "libxslt1-dev", ''
+depend :remote, :deb, "sphinxsearch", ''
