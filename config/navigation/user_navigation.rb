f2daa87dccf7fdb4d60c05202209453eb6c52200
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-04-27 01:39:00 +0200

    Split settings class and user menu.

--- config/navigation/user_navigation.rb
+++ config/navigation/user_navigation.rb
@@ -16,8 +16,6 @@ SimpleNavigation::Configuration.run do |navigation|
       settings.item :divider_three,             "", :class => 'divider'
       settings.item :account_types,             t('bookyt.settings.account_types.title'), account_types_path
       settings.item :booking_templates,         t('bookyt.settings.booking_templates.title'), booking_templates_path
-      settings.item :divider_four,              "", :class => 'divider'
-      settings.item :logout,                    t('bookyt.main_navigation.destroy_user_session'), destroy_user_session_path, :method => :delete, :highlights_on => /\/users\/sign_out/, :if => Proc.new { user_signed_in? }
     end
 
     primary.dom_class = 'nav secondary-nav'
