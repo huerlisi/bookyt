0d22086a7001044d46c69224faec7bdb5069ee28
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-04-29 13:46:16 +0200

    Drop tooltips from navigation.

--- config/navigation/main_navigation.rb
+++ config/navigation/main_navigation.rb
@@ -13,22 +13,17 @@ SimpleNavigation::Configuration.run do |navigation|
   # Define the primary navigation
   navigation.items do |primary|
     primary.item :accounting, t('bookyt.main_navigation.accounting'), '#',
-                 :tooltip => t('bookyt.main_navigation.tooltip.accounting'),
                  :if => Proc.new { user_signed_in? } do |accounting|
       accounting.item :booking, t('bookyt.main_navigation.new_booking'), new_booking_path,
-                      :tooltip => t('bookyt.main_navigation.tooltip.new_booking'),
                       :highlights_on => /\/bookings\/.*$/
       accounting.item :booking, t('bookyt.main_navigation.new_expense'), new_expense_path
       accounting.item :accounting_divider, "", :class => 'divider'
       accounting.item :accounts, t_title(:index, Account), accounts_path
       accounting.item :bookings, t('bookyt.main_navigation.bookings'), bookings_path,
-                      :tooltip => t('bookyt.main_navigation.tooltip.bookings'),
                       :highlights_on => /\/bookings($|\?)/
       accounting.item :balance, t('bookyt.main_navigation.balance'), balance_sheet_tenant_path(current_user.tenant, :by_value_period => params[:by_value_period]),
-                      :tooltip => t('bookyt.main_navigation.tooltip.balance'),
                       :highlights_on => /#{Regexp.escape(balance_sheet_tenant_path(current_user.tenant))}($|\?)/
       accounting.item :profit, t('bookyt.main_navigation.profit'), profit_sheet_tenant_path(current_user.tenant, :by_value_period => params[:by_value_period]),
-                      :tooltip => t('bookyt.main_navigation.tooltip.profit'),
                       :highlights_on => /#{Regexp.escape(profit_sheet_tenant_path(current_user.tenant))}($|\?)/
       accounting.item :divider, "", :class => 'divider'
       accounting.item :mt940_import, t_title(:import, BookingImportAttachment), new_booking_import_attachment_path
@@ -50,25 +45,18 @@ SimpleNavigation::Configuration.run do |navigation|
     end
 
     primary.item :contacts, t('bookyt.main_navigation.contacts'), '#',
-                 :tooltip => t('bookyt.main_navigation.tooltip.contacts'),
                  :if => Proc.new { user_signed_in? } do |contacts|
       contacts.item :people, t_title(:index, Person), people_path,
-                             :tooltip => t('bookyt.main_navigation.tooltip.people'),
                              :highlights_on => /\/people($|\?|\/[0-9]*($|\?|\/.*))/
       contacts.item :employees, t_title(:index, Employee), employees_path,
-                             :tooltip => t('bookyt.main_navigation.tooltip.employees'),
                              :highlights_on => /\/employees($|\?|\/[0-9]*($|\?|\/.*))/
       contacts.item :new_employee, t_title(:new, Employee), new_employee_path,
-                             :tooltip => t('bookyt.main_navigation.tooltip.new_employee'),
                              :highlights_on => /\/employees\/new$/
       contacts.item :customers, t_title(:index, Customer), customers_path,
-                             :tooltip => t('bookyt.main_navigation.tooltip.customers'),
                              :highlights_on => /\/customers($|\?|\/[0-9]*($|\?|\/.*))/
       contacts.item :new_customer, t_title(:new, Customer), new_customer_path,
-                             :tooltip => t('bookyt.main_navigation.tooltip.new_customer'),
                              :highlights_on => /\/customers\/new$/
       contacts.item :companies, t_title(:index, Company), companies_path,
-                             :tooltip => t('bookyt.main_navigation.tooltip.companies'),
                              :highlights_on => /\/companies($|\?|\/[0-9]*($|\?|\/.*))/
     end
 
