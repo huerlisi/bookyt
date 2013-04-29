60eb80dd76e68c2117a450e3baa503edba7c0fdd
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-04-29 11:26:20 +0200

    Refactor main menu; make debit and credit invoices top menu items.

--- config/navigation/main_navigation.rb
+++ config/navigation/main_navigation.rb
@@ -9,29 +9,6 @@ SimpleNavigation::Configuration.run do |navigation|
 
   # Define the primary navigation
   navigation.items do |primary|
-    primary.item :contacts, t('bookyt.main_navigation.contacts'), '#',
-                 :tooltip => t('bookyt.main_navigation.tooltip.contacts'),
-                 :if => Proc.new { user_signed_in? } do |contacts|
-      contacts.item :people, t_title(:index, Person), people_path,
-                             :tooltip => t('bookyt.main_navigation.tooltip.people'),
-                             :highlights_on => /\/people($|\?|\/[0-9]*($|\?|\/.*))/
-      contacts.item :employees, t_title(:index, Employee), employees_path,
-                             :tooltip => t('bookyt.main_navigation.tooltip.employees'),
-                             :highlights_on => /\/employees($|\?|\/[0-9]*($|\?|\/.*))/
-      contacts.item :new_employee, t_title(:new, Employee), new_employee_path,
-                             :tooltip => t('bookyt.main_navigation.tooltip.new_employee'),
-                             :highlights_on => /\/employees\/new$/
-      contacts.item :customers, t_title(:index, Customer), customers_path,
-                             :tooltip => t('bookyt.main_navigation.tooltip.customers'),
-                             :highlights_on => /\/customers($|\?|\/[0-9]*($|\?|\/.*))/
-      contacts.item :new_customer, t_title(:new, Customer), new_customer_path,
-                             :tooltip => t('bookyt.main_navigation.tooltip.new_customer'),
-                             :highlights_on => /\/customers\/new$/
-      contacts.item :companies, t_title(:index, Company), companies_path,
-                             :tooltip => t('bookyt.main_navigation.tooltip.companies'),
-                             :highlights_on => /\/companies($|\?|\/[0-9]*($|\?|\/.*))/
-    end
-
     primary.item :accounting, t('bookyt.main_navigation.accounting'), '#',
                  :tooltip => t('bookyt.main_navigation.tooltip.accounting'),
                  :if => Proc.new { user_signed_in? } do |accounting|
@@ -56,22 +33,42 @@ SimpleNavigation::Configuration.run do |navigation|
       accounting.item :mt940_import, t_title(:import, BookingImportAttachment), new_booking_import_attachment_path
     end
 
-    primary.item :invoicing, t('bookyt.main_navigation.invoicing'), '#',
-                 :tooltip => t('bookyt.main_navigation.tooltip.invoicing'),
-                 :if => Proc.new { user_signed_in? },
-                 :highlights_on => /\/(credit_|debit_)?invoices.*/ do |invoicing|
-      invoicing.item :credit_invoices, t('bookyt.main_navigation.credit_invoices'), credit_invoices_path,
-                     :highlights_on => /\/credit_invoices$/
-      invoicing.item :new_credit_invoice, t('bookyt.main_navigation.new_credit_invoice'), new_credit_invoice_path,
-                     :highlights_on => /\/credit_invoices\/new$/
-      invoicing.item :divider, "", :class => 'divider'
-      invoicing.item :debit_invoices, t('bookyt.main_navigation.debit_invoices'), debit_invoices_path,
-                     :highlights_on => /\/debit_invoices$/
-      invoicing.item :new_debit_invoice, t('bookyt.main_navigation.new_debit_invoice'), new_debit_invoice_path,
-                     :highlights_on => /\/debit_invoices\/new$/
+    primary.item :debit_invoices, t_title(:index, DebitInvoice), '#',
+                 :if => Proc.new { user_signed_in? } do |invoicing|
+      invoicing.item :new_debit_invoice, t('bookyt.main_navigation.new_debit_invoice'), new_debit_invoice_path
+      invoicing.item :debit_invoices, t('bookyt.main_navigation.debit_invoices'), debit_invoices_path
       invoicing.item :divider, "", :class => 'divider'
       invoicing.item :new_esr_file, t('bookyt.main_navigation.new_esr_file'), new_esr_file_path
       invoicing.item :esr_files, t('bookyt.main_navigation.esr_files'), esr_files_path
+    end
+
+    primary.item :credit_invoices, t_title(:index, CreditInvoice), '#',
+                 :if => Proc.new { user_signed_in? } do |invoicing|
+      invoicing.item :new_credit_invoice, t('bookyt.main_navigation.new_credit_invoice'), new_credit_invoice_path
+      invoicing.item :credit_invoices, t('bookyt.main_navigation.credit_invoices'), credit_invoices_path
+    end
+
+    primary.item :contacts, t('bookyt.main_navigation.contacts'), '#',
+                 :tooltip => t('bookyt.main_navigation.tooltip.contacts'),
+                 :if => Proc.new { user_signed_in? } do |contacts|
+      contacts.item :people, t_title(:index, Person), people_path,
+                             :tooltip => t('bookyt.main_navigation.tooltip.people'),
+                             :highlights_on => /\/people($|\?|\/[0-9]*($|\?|\/.*))/
+      contacts.item :employees, t_title(:index, Employee), employees_path,
+                             :tooltip => t('bookyt.main_navigation.tooltip.employees'),
+                             :highlights_on => /\/employees($|\?|\/[0-9]*($|\?|\/.*))/
+      contacts.item :new_employee, t_title(:new, Employee), new_employee_path,
+                             :tooltip => t('bookyt.main_navigation.tooltip.new_employee'),
+                             :highlights_on => /\/employees\/new$/
+      contacts.item :customers, t_title(:index, Customer), customers_path,
+                             :tooltip => t('bookyt.main_navigation.tooltip.customers'),
+                             :highlights_on => /\/customers($|\?|\/[0-9]*($|\?|\/.*))/
+      contacts.item :new_customer, t_title(:new, Customer), new_customer_path,
+                             :tooltip => t('bookyt.main_navigation.tooltip.new_customer'),
+                             :highlights_on => /\/customers\/new$/
+      contacts.item :companies, t_title(:index, Company), companies_path,
+                             :tooltip => t('bookyt.main_navigation.tooltip.companies'),
+                             :highlights_on => /\/companies($|\?|\/[0-9]*($|\?|\/.*))/
     end
 
     # Hack to get engine navigations included
