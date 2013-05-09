f8f4494672feed65d0c19d7907d05445654ef10c
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-05-09 23:12:03 +0200

    Refactor Tenant to be closer to Hozr etc.

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,32 +1,39 @@
 class Tenant < ActiveRecord::Base
-  # Associations
-  belongs_to :company, :foreign_key => :person_id
-  accepts_nested_attributes_for :company
-  has_many :users
+  # Person
+  # ======
+  belongs_to :person
+  accepts_nested_attributes_for :person
+  attr_accessible :person_attributes
+  def person_with_autobuild
+    person_without_autobuild || build_person
+  end
+  alias_method_chain :person, :autobuild
 
   # Settings
+  # ========
   has_settings
+  attr_accessible :settings
 
-  def payment_period
-    settings.payment_period / 24 / 3600
-  end
-  def payment_period=(value)
-    settings.payment_period = value.to_i.days
-  end
-
-
-  # Validations
-  validates_presence_of :company
-  validates_date :incorporated_on
-
-  # String
   def to_s
-    company.to_s
+    person.to_s
   end
+
+  # User
+  # ====
+  has_many :users
+  attr_accessible :user_ids
+
+  # Company
+  belongs_to :company, :foreign_key => :person_id
+  validates_presence_of :company
+  accepts_nested_attributes_for :company
+
+  # Bookyt
+  # ======
 
   # Fiscal Years
-  # ============
   validates_date :fiscal_year_ends_on
+  validates_date :incorporated_on
 
   def fiscal_period(year)
     final_day_of_fiscal_year = Date.new(year, fiscal_year_ends_on.month, fiscal_year_ends_on.day)
@@ -44,8 +51,17 @@ class Tenant < ActiveRecord::Base
     }
   end
 
+  # Vat
   def vat_obligation?
     vat_number.present?
+  end
+
+  # Settings
+  def payment_period
+    settings.payment_period / 24 / 3600
+  end
+  def payment_period=(value)
+    settings.payment_period = value.to_i.days
   end
 
   # Attachments
