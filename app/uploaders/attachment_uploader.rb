eaafeae287075c33a798b45f7245f28bac31ecd5
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-05-30 10:03:10 +0200

    Use tenant namespace when persisting uploads.

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -7,6 +7,6 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    root.join(model.class.to_s.underscore, mounted_as.to_s, model.id.to_s)
+    root.join('tenant', Apartment::Database.current, model.class.to_s.underscore, mounted_as.to_s, model.id.to_s)
   end
 end
