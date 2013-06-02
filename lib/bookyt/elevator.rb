a2b49457e4c3240d3039fbf1f12c560a1367e326
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-06-02 02:38:09 +0200

    Use db_name column to detect db for subdomain.

--- lib/bookyt/elevator.rb
+++ lib/bookyt/elevator.rb
@@ -1,8 +1,8 @@
 class Bookyt::Elevator < Apartment::Elevators::Subdomain
   def parse_database_name(request)
-    database_name = super
-    if Apartment.database_names.include? database_name
-      return database_name
+    subdomain = super
+    if tenant = Tenant.find_by_subdomain(subdomain)
+      return tenant.db_name
     else
       return nil
     end
