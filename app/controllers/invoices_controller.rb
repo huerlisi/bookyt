f616cb68f3d527905f27cd9209d53231945d0d54
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-06-19 15:06:46 +0200

    No need for generic invoices#new action.

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -21,15 +21,6 @@ class InvoicesController < AuthorizedController
     index!
   end
 
-  # Actions
-  def new
-    invoice_params = params[:invoice] || {}
-    invoice_params.merge!(:company_id => current_tenant.company.id)
-    @invoice = Invoice.new(invoice_params)
-
-    new!
-  end
-
   def letter
     show!
   end
