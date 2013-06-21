89856cc6f6ed67cb1c1e3bde46f13d5636957fa4
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-06-21 23:23:05 +0200

    New migration to port engine settings to DB.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130620114559) do
+ActiveRecord::Schema.define(:version => 20130620221230) do
 
   create_table "account_types", :force => true do |t|
     t.string   "name",       :limit => 100
