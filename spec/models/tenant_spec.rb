3ce8ac2a43aeedb14c56d20a230c18622abb2109
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-08-16 13:35:21 +0200

    Fix tenant validation specs.

--- spec/models/tenant_spec.rb
+++ spec/models/tenant_spec.rb
@@ -3,12 +3,14 @@ require 'spec_helper'
 describe Tenant do
   it { should belong_to :company }
 
-  it { should validate_presence_of :company }
-  #it { should validate_date :incorporated_on } # TODO: This validations can be checked at the moment
+  context "on update" do
+    subject { Factory.create :tenant }
+
+    it { should validate_presence_of :company }
+    pending { should validate_date :incorporated_on } # TODO: This validations can be checked at the moment
+  end
 
   context "when new" do
-    specify { should_not be_valid }
-
     its(:to_s) { should == "" }
   end
 
