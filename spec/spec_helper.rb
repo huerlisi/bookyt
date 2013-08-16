bb646d883babb6ed18a751c073b31192a1ed757f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-08-16 23:09:21 +0200

    Fix fixtures cleaning for feature specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,9 +37,19 @@ RSpec.configure do |config|
   # config.use_transactional_examples = true
 
   require 'database_cleaner'
+
+  config.use_transactional_fixtures                      = false
+
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :feature) do
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each) do
