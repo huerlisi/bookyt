8bbad3fcfe5096fdd362771eebcbfe7880ae232a
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-08-16 12:57:02 +0200

    Add guard command to zeus.

--- custom_plan.rb
+++ custom_plan.rb
@@ -2,6 +2,11 @@ require 'zeus/rails'
 
 class CustomPlan < Zeus::Rails
 
+  def guard
+    require 'guard/cli'
+    Guard::CLI.start
+  end
+
   # def my_custom_command
   #  # see https://github.com/burke/zeus/blob/master/docs/ruby/modifying.md
   # end
