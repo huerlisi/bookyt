2ddde24fb88d009dae1a63245dd83fc24c827fb4
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2014-02-12 09:36:34 +0100

    Bump cookie_store session id.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Bookyt::Application.config.session_store :cookie_store, :key => '_bookyt_session2'
+Bookyt::Application.config.session_store :cookie_store, :key => '_bookyt_session3'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
