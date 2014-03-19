bbf282ccaff09b6c764ff571069c531f9df0b0d8
Simon Hürlimann <simon.huerlimann@cyt.ch>
2014-03-19 11:03:25 +0100

    Set i18n.enforce_available_locales to fix deprecation warning.

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module Bookyt
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    config.i18n.enforce_available_locales = true
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.fallbacks = [:'de-CH', :de]
     config.i18n.default_locale = 'de-CH'
