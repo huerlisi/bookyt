25a2456a4ebc982e32c48a782b458e4eb05be444
Simon Hürlimann <simon.huerlimann@cyt.ch>
2014-03-24 18:04:08 +0100

    Update has_accounts_engine gem.
    This mostly fixes the broken document tag for accounts.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,11 @@
+# Account model decoration
+#
+# We use this file to decorate Account models with attachments.
+
+# Explicit load of Account model from has_accounts gem.
 require HasAccounts::Railtie.config.root.join('app', 'models', 'account')
 
+# Reopen class do add decorations
 Account.class_eval do
   # Attachments
   # ===========
