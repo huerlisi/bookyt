7ac8253ee84bcb6185d6200572683ada6f632af8
Simon Hürlimann <simon.huerlimann@cyt.ch>
2014-03-24 22:31:45 +0100

    Update i18n_rails_helpers and drop action views for admin tenant.

--- app/controllers/admin/tenants_controller.rb
+++ app/controllers/admin/tenants_controller.rb
@@ -9,6 +9,11 @@ class Admin::TenantsController < ApplicationController
     @current_ability ||= Ability.new(current_admin_user)
   end
 
+  # Redirect to the called path before the login
+  def after_sign_in_path_for(resource)
+      (session[:"user.return_to"].nil?) ? "/" : session[:"user.return_to"].to_s
+  end
+
   # Actions
   # =======
   def new
@@ -41,4 +46,10 @@ class Admin::TenantsController < ApplicationController
       render 'new'
     end
   end
+
+  # Resource setup
+  protected
+    def collection
+      instance_eval("@#{controller_name.pluralize} ||= end_of_association_chain.accessible_by(current_ability, :list).paginate(:page => params[:page], :per_page => params[:per_page])")
+    end
 end
