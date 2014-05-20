0aea2ae9b74469cb73ebf3aa24b2f669ad65d94b
Simon Hürlimann <simon.huerlimann@cyt.ch>
2014-05-20 17:29:36 +0200

    Support importing backup files.

--- app/controllers/backups_controller.rb
+++ app/controllers/backups_controller.rb
@@ -2,12 +2,24 @@
 class BackupsController < AttachmentsController
   defaults :resource_class => Backup
 
+  def create
+    @backup = Backup.new(params[:backup])
+    if @backup.save
+      @backup.import
+
+      redirect_to current_tenant
+    else
+      render :new
+    end
+  end
+
   def dump
     tenant = Tenant.find(params[:tenant_id])
 
     @backup = tenant.backups.build
     @backup.dump
     @backup.save!
+
     redirect_to current_tenant
   end
 
@@ -15,6 +27,7 @@ class BackupsController < AttachmentsController
     # TODO: access validation
     @backup = Backup.find(params[:id])
     @backup.import
+
     redirect_to current_tenant
   end
 end
