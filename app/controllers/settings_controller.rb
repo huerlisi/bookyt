fb4e24b10ea4739fa7aa27d4b04fb430bb2cd593
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2015-03-09 17:59:25 +0100

    Handle case of no vesr account configured in vesr settings.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -10,8 +10,6 @@ class SettingsController < ApplicationController
   end
 
   def update_vesr
-    @tenant          = Tenant.find(params[:vesr][:tenant][:id])
-    @bank_account    = BankAccount.find(params[:vesr][:bank_account][:id])
     if params[:vesr][:attachment][:id].present?
       @letter_template = Attachment.find(params[:vesr][:attachment][:id])
       @letter_template.update_attributes(params[:vesr][:letter_template])
@@ -19,8 +17,13 @@ class SettingsController < ApplicationController
       @letter_template = Attachment.new(params[:vesr][:attachment], :code => 'Prawn::LetterDocument')
     end
 
+    @tenant = Tenant.find(params[:vesr][:tenant][:id])
     @tenant.update_attributes(params[:vesr][:tenant])
-    @bank_account.update_attributes(params[:vesr][:bank_account])
+
+    if params[:vesr][:bank_account]
+      @bank_account = BankAccount.find(params[:vesr][:bank_account][:id])
+      @bank_account.update_attributes(params[:vesr][:bank_account])
+    end
 
     render :action => 'vesr'
   end
