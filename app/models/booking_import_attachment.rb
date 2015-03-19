d5f76c1ca657db4f2277eaa0bf4a6097f70d6940
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2015-03-19 16:54:20 +0100

    Fix .line endings in MT940 files before parsing.

--- app/models/booking_import_attachment.rb
+++ app/models/booking_import_attachment.rb
@@ -4,6 +4,6 @@ class BookingImportAttachment < Attachment
   accepts_nested_attributes_for :booking_import
 
   def content
-    File.read(file.current_path, :encoding => self.encoding)
+    File.read(file.current_path, :encoding => self.encoding).encode(universal_newline: true)
   end
 end
