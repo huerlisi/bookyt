488800abc9b92957cde550eaa835cfb23d75571f
Severin Ulrich <sulrich@downloads.ch>
2015-05-09 03:36:32 +0200

    Updated Tenant Query Code in Apartement Elevator to be Rails 4.2 compatible.

--- lib/bookyt/elevator.rb
+++ lib/bookyt/elevator.rb
@@ -3,7 +3,7 @@ require 'apartment/elevators/subdomain'
 class Bookyt::Elevator < Apartment::Elevators::Subdomain
   def subdomain(request)
     subdomain = super
-    if tenant = Admin::Tenant.find_by_subdomain(subdomain)
+    if tenant = Admin::Tenant.where(subdomain: subdomain).first
       return tenant.db_name
     else
       return nil
