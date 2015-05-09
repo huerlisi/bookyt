cdd246e933beb346122030c9c38f04f0a7357c1f
Severin Ulrich <sulrich@downloads.ch>
2015-05-09 04:00:57 +0200

    Add fix for SetInvoiceVesrTag migration on empty dbs

--- db/migrate/20150309165055_set_invoice_vesr_tag.rb
+++ db/migrate/20150309165055_set_invoice_vesr_tag.rb
@@ -3,7 +3,10 @@ class SetInvoiceVesrTag < ActiveRecord::Migration
     return unless BankAccount.tagged_with('invoice:vesr')
 
     account = BankAccount.find_by_code('1020')
-    account.tag_list << 'invoice:vesr'
-    account.save!
+    if account
+      account.tag_list << 'invoice:vesr'
+      account.save!
+      logger.info "Added tag 'invoice:vesr' to Bank Account with code '1020'"
+    end
   end
 end
