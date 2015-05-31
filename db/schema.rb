dc8f79925bc374b21fe4035000ac1bea7c4739a9
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2015-05-31 10:17:23 +0200

    fix(schema): update schema.rb after clean db:migrate run
    The schema.rb should reflect the state of the database after all migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -499,15 +499,12 @@ ActiveRecord::Schema.define(:version => 20150309165055) do
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], :name => "taggings_idx", :unique => true
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
   add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
 
   create_table "tags", :force => true do |t|
-    t.string  "name"
-    t.integer "taggings_count", :default => 0
+    t.string "name"
   end
-
-  add_index "tags", ["name"], :name => "index_tags_on_name", :unique => true
 
   create_table "tenants", :force => true do |t|
     t.integer  "person_id"
