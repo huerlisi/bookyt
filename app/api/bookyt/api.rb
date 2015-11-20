e80e46283ddb86d37b40aa3ace0c6d9d73e88fd2
Raffael Schmid <raffael@yux.ch>
2015-11-20 12:13:07 +0100

    Return validation errors in a consistent way

--- app/api/bookyt/api.rb
+++ app/api/bookyt/api.rb
@@ -13,12 +13,8 @@ module Bookyt
     end
 
     rescue_from ActiveRecord::RecordInvalid do |error|
-      error = {
-        'status'  => 422,
-        'message' => error.record.errors.full_messages.to_sentence,
-        'errors'  => error.record.errors,
-      }
-      Rack::Response.new(error.to_json, 422)
+      message = { 'error' => error.record.errors.full_messages }
+      Rack::Response.new(message.to_json, 422)
     end
 
     mount Bookyt::API::Bookings
