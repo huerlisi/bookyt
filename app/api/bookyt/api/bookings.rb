a6bec91dd1a8ff559479650c815226eb0f58b63f
Raffael Schmid <raffael@yux.ch>
2015-11-25 16:53:09 +0100

    Add Booking update endpoint

--- app/api/bookyt/api/bookings.rb
+++ app/api/bookyt/api/bookings.rb
@@ -43,6 +43,35 @@ module Bookyt
             present booking, with: Bookyt::Entities::Booking
           end
 
+          desc 'Update a booking'
+          params do
+            requires :title, type: String, desc: 'Title of the booking'
+            requires :amount, type: BigDecimal, desc: 'Amount to be booked'
+            requires :value_date, type: Date, desc: 'Date of the booking'
+            requires :credit_account_tag, type: String, desc: 'Tag of the credit account'
+            requires :debit_account_tag, type: String, desc: 'Tag of the debit account'
+            optional :comments, type: String, desc: 'Additional comments'
+          end
+          put do
+            begin
+              credit_account = Account.find_by_tag(params[:credit_account_tag])
+              debit_account = Account.find_by_tag(params[:debit_account_tag])
+              attributes = {
+                title: params[:title],
+                amount: params[:amount],
+                value_date: params[:value_date],
+                credit_account: credit_account,
+                debit_account: debit_account,
+                comments: params[:comments],
+              }
+              booking = Booking.find(params[:id])
+              booking.update_attributes!(attributes)
+              present booking, with: Bookyt::Entities::Booking
+            rescue Account::AmbiguousTag => error
+              error!({ error: error.to_s }, 422)
+            end
+          end
+
           desc 'Delete a booking'
           delete do
             Booking.find(params[:id]).destroy
--- spec/api/bookyt/api/bookings_spec.rb
+++ spec/api/bookyt/api/bookings_spec.rb
@@ -92,6 +92,59 @@ RSpec.describe Bookyt::API::Bookings, type: :request do
     end
   end
 
+  describe 'PUT /api/bookings/:id' do
+    let(:params) do
+      {
+        title: 'Test',
+        amount: 13.37,
+        value_date: Date.today,
+        credit_account_tag: 'incoming:test:credit',
+        debit_account_tag: 'incoming:test:debit',
+        comments: 'This is a loooooooooooong comment',
+      }
+    end
+    let!(:booking) { FactoryGirl.create :account_booking }
+
+    context 'accounts present' do
+      before do
+        FactoryGirl.create(:account, tag_list: 'incoming:test:credit')
+        FactoryGirl.create(:account, tag_list: 'incoming:test:debit')
+      end
+
+      it 'returns the created booking' do
+        put "/api/bookings/#{booking.id}", params, headers
+        expect(JSON.parse(response.body)).to be_instance_of(Hash)
+        expect(response.status).to eq(200)
+      end
+
+      it 'creates a new booking' do
+        expect { put "/api/bookings/#{booking.id}", params, headers }.to change { booking.reload.amount }
+      end
+
+      it 'uses Bookyt::Entities::Booking to display the created Booking' do
+        expect(Bookyt::Entities::Booking).to receive(:represent)
+        put "/api/bookings/#{booking.id}", params, headers
+      end
+    end
+
+    context 'too many accounts found' do
+      it 'returns an error' do
+        expect(Account).to receive(:find_by_tag).and_raise(Account::AmbiguousTag.new('asd', 2))
+        put "/api/bookings/#{booking.id}", params, headers
+        expect(JSON.parse(response.body)).to be_instance_of(Hash)
+        expect(response.status).to eq(422)
+      end
+    end
+
+    context 'an account cannot be found' do
+      it 'returns an error' do
+        put "/api/bookings/#{booking.id}", params, headers
+        expect(JSON.parse(response.body)).to be_instance_of(Hash)
+        expect(response.status).to eq(422)
+      end
+    end
+  end
+
   describe 'DELETE /api/bookings/:id' do
     let(:params) { {} }
     let!(:booking) { FactoryGirl.create(:account_booking) }
