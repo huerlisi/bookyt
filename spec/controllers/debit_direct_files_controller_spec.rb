3cabe16d685b36a27342b4caef0f50b9e8739638
Raffael Schmid <raffael@yux.ch>
2016-01-25 17:24:58 +0100

    fix specs

--- spec/controllers/debit_direct_files_controller_spec.rb
+++ spec/controllers/debit_direct_files_controller_spec.rb
@@ -4,9 +4,12 @@ RSpec.describe DebitDirectFilesController, :type => :controller do
   login_admin
 
   describe 'POST create' do
+    let!(:bank_account) { FactoryGirl.create(:bank_account, tag_list: 'invoice:vesr') }
+    let(:debit_direct_file) { FactoryGirl.create(:debit_direct_file) }
+
     it 'uses DebitDirectFileCreator.call to create a DebitDirectFile' do
       expect(DebitDirectFileCreator).
-        to receive(:call).with(instance_of(Tenant), instance_of(BankAccount)).and_return(true)
+        to receive(:call).with(instance_of(Tenant), bank_account).and_return(debit_direct_file)
       post :create
     end
   end
